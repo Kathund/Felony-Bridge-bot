88d39cc8b29274b939fec3394c5601322379e065
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-11 20:25:54 +0800

    Create skywarsStatsCommand.js

---
+++ src/minecraft/commands/skywarsStatsCommand.js
@@ -0,0 +1,49 @@
+const MinecraftCommand = require('../../contracts/MinecraftCommand')
+const HypixelAPIReborn = require('hypixel-api-reborn')
+const HypAPI = new HypixelAPIReborn.Client('cc4cde34-734e-4b33-9bc6-51c65006ccff')
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+class PingCommand extends MinecraftCommand {
+  constructor(minecraft) {
+    super(minecraft)
+
+    this.name = 'sw'
+    this.aliases = ['skywars']
+    this.description = 'Look up your skywars stats'
+  }
+
+ 
+  onCommand(username, message) {
+    // get the player name in the second word of the message
+    const player = message.split(' ')[1]
+    // get the player's stats
+    HypAPI.getPlayer(player).then((data) => {
+        // check the ammount of splits in the message
+        if(message.split(' ').length == 2) {
+          this.send(`/gc Info for ${player} - Level: ${data.stats.skywars.level}, Kills: ${data.stats.skywars.kills}, Wins: ${data.stats.skywars.wins}, KD: ${data.stats.skywars.KDRatio}, WLR: ${data.stats.skywars.WLRatio},- ${makeid(10)}`)
+        }
+        else if (message.split(' ').length == 3) {
+          const mode = message.split(' ')[2]
+          if (mode == 'solo') {
+            if (type == 'overall' || type == 'insane' || type == 'normal' ) {
+              this.send(`/gc Info for ${player} in mode ${mode} - Level: ${data.stats.skywars.level}, Kills: ${data.stats.skywars.kills}, Wins: ${data.stats.skywars.wins}, KD: ${data.stats.skywars.KDRatio}, WLR: ${data.stats.skywars.WLRatio},- ${makeid(10)}`)
+            }
+          }
+        }
+    })
+  }
+}
+
+module.exports = PingCommand
+
+
