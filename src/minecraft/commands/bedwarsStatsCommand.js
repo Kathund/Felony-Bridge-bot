75740c1678470c19e6d394a9863c70e40c37c3dc
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-11 19:22:29 +0800

    Update bedwarsStatsCommand.js

--- src/minecraft/commands/bedwarsStatsCommand.js
+++ src/minecraft/commands/bedwarsStatsCommand.js
@@ -39,14 +39,9 @@ class PingCommand extends MinecraftCommand {
             mode == 'castle') {
               this.send(`/gc Info for ${player} in mode ${mode} - FKDR: ${data.stats.bedwars[mode].finalKDRatio}, Winstreak: ${data.stats.bedwars[mode].winstreak}, Wins: ${data.stats.bedwars[mode].wins}, WLR ${data.stats.bedwars[mode].WLRatio}, BLR ${data.stats.bedwars[mode].beds.BLRatio} - ${makeid(10)}`)
           }
-          // else if (mode == 'men') {
-          //   console.log(data.stats.skywars)
-          // }
           else {
             this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle] - ${makeid(10)}`)
           }
-          // console.log(data.stats.bedwars)
-          // this.send(`/gc Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins} - ${makeid(10)}`)
         }
         else {
           this.send(`/gc Info for ${player} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio} - ${makeid(10)}`)
