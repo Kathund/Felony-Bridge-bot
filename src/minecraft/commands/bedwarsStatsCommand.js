15ec5d4974cb09fe090fa542cbad300157986852
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-11 20:26:02 +0800

    Update bedwarsStatsCommand.js

--- src/minecraft/commands/bedwarsStatsCommand.js
+++ src/minecraft/commands/bedwarsStatsCommand.js
@@ -37,14 +37,14 @@ class PingCommand extends MinecraftCommand {
             mode == 'fours' ||
             mode == '4v4' ||
             mode == 'castle') {
-              this.send(`/gc Info for ${player} in mode ${mode} - FKDR: ${data.stats.bedwars[mode].finalKDRatio}, Winstreak: ${data.stats.bedwars[mode].winstreak}, Wins: ${data.stats.bedwars[mode].wins}, WLR ${data.stats.bedwars[mode].WLRatio}, BLR ${data.stats.bedwars[mode].beds.BLRatio} - ${makeid(10)}`)
+              this.send(`/gc Info for ${player} in mode ${mode} - FKDR: ${data.stats.bedwars[mode].finalKDRatio}, Winstreak: ${data.stats.bedwars[mode].winstreak}, Wins: ${data.stats.bedwars[mode].wins}, WLR ${data.stats.bedwars[mode].WLRatio}, BLR ${data.stats.bedwars[mode].beds.BLRatio}, Finals: ${data.stats.bedwars[mode].finalKills}, Beds: ${data.stats.bedwars[mode].beds.broken} - ${makeid(10)}`)
           }
           else {
             this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle] - ${makeid(10)}`)
           }
         }
         else {
-          this.send(`/gc Info for ${player} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio} - ${makeid(10)}`)
+          this.send(`/gc Info for ${player} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
         }
     }).catch((err) => {
       this.send(`/gc ${player} dose not exist! - ${makeid(10)}`)
