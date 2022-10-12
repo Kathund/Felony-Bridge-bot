e56dc3549e6f6c8d5e813f03d809cd98546c28a2
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-12 17:18:06 +0800

    Update bedwarsStatsCommand.js

--- src/minecraft/commands/bedwarsStatsCommand.js
+++ src/minecraft/commands/bedwarsStatsCommand.js
@@ -1,6 +1,7 @@
 const MinecraftCommand = require('../../contracts/MinecraftCommand')
 const HypixelAPIReborn = require('hypixel-api-reborn')
 const HypAPI = new HypixelAPIReborn.Client('cc4cde34-734e-4b33-9bc6-51c65006ccff')
+const check = 'enabled'
 
 function makeid(length) {
     var result           = '';
@@ -13,7 +14,7 @@ function makeid(length) {
    return result;
 }
 
-class PingCommand extends MinecraftCommand {
+class BedwarsCommand extends MinecraftCommand {
   constructor(minecraft) {
     super(minecraft)
 
@@ -24,36 +25,41 @@ class PingCommand extends MinecraftCommand {
 
  
   onCommand(username, message) {
-    // get the player name in the second word of the message
-    const player = message.split(' ')[1]
-    // get the player's stats
-    HypAPI.getPlayer(player).then((data) => {
-        // check the ammount of splits in the message
-        if(message.split(' ').length != 2) {
-          const mode = message.split(' ')[2]
-          if(mode == 'solo' ||
-            mode == 'doubles' ||
-            mode == 'threes' ||
-            mode == 'fours' ||
-            mode == '4v4' ||
-            mode == 'castle') {
-              this.send(`/gc Info for ${player} in mode ${mode} - FKDR: ${data.stats.bedwars[mode].finalKDRatio}, Winstreak: ${data.stats.bedwars[mode].winstreak}, Wins: ${data.stats.bedwars[mode].wins}, WLR ${data.stats.bedwars[mode].WLRatio}, BLR ${data.stats.bedwars[mode].beds.BLRatio}, Finals: ${data.stats.bedwars[mode].finalKills}, Beds: ${data.stats.bedwars[mode].beds.broken} - ${makeid(10)}`)
+    if (check != 'disabled') {
+      // get the player name in the second word of the message
+      const player = message.split(' ')[1]
+      // get the player's stats
+      HypAPI.getPlayer(player).then((data) => {
+          // check the ammount of splits in the message
+          if(message.split(' ').length != 2) {
+            const mode = message.split(' ')[2]
+            if(mode == 'solo' ||
+              mode == 'doubles' ||
+              mode == 'threes' ||
+              mode == 'fours' ||
+              mode == '4v4' ||
+              mode == 'castle') {
+                this.send(`/gc Info for ${player} in mode ${mode} - FKDR: ${data.stats.bedwars[mode].finalKDRatio}, Winstreak: ${data.stats.bedwars[mode].winstreak}, Wins: ${data.stats.bedwars[mode].wins}, WLR ${data.stats.bedwars[mode].WLRatio}, BLR ${data.stats.bedwars[mode].beds.BLRatio}, Finals: ${data.stats.bedwars[mode].finalKills}, Beds: ${data.stats.bedwars[mode].beds.broken} - ${makeid(10)}`)
+            }
+            else {
+              this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle] - ${makeid(10)}`)
+            }
           }
           else {
-            this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle] - ${makeid(10)}`)
+            this.send(`/gc Info for ${player} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
           }
-        }
-        else {
-          this.send(`/gc Info for ${player} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
-        }
-    }).catch((err) => {
-      this.send(`/gc ${player} dose not exist! - ${makeid(10)}`)
-      console.log(`error was caused by ${username}`)
-      console.log(err)
-    })
+      }).catch((err) => {
+        this.send(`/gc ${player} dose not exist! - ${makeid(10)}`)
+        console.log(`error was caused by ${username}`)
+        console.log(err)
+      })
+    }
+    else {
+      this.send(`/gc This command is disabled! - ${makeid(10)}`)
+    }
   }
 }
 
-module.exports = PingCommand
+module.exports = BedwarsCommand
 
 
