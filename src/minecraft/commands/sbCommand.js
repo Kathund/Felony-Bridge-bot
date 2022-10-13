ccd46ad37707d7d4a8a20a0b259e0c2c5139f3ee
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-13 20:29:07 +0800

    Update sbCommand.js

--- src/minecraft/commands/sbCommand.js
+++ src/minecraft/commands/sbCommand.js
@@ -1,7 +1,10 @@
+// TODO once they update the package u can work on this dumb ass fucking shit or go die in in a hole
+// TODO you are useless and u should die in a fucking hole 
+
 const MinecraftCommand = require('../../contracts/MinecraftCommand')
 const HypixelAPIReborn = require('hypixel-api-reborn')
 const HypAPI = new HypixelAPIReborn.Client('cc4cde34-734e-4b33-9bc6-51c65006ccff')
-const check = 'disabled'
+const check = 'test'
 
 function makeid(length) {
     var result           = '';
@@ -24,28 +27,31 @@ class sbCommand extends MinecraftCommand {
   }
 
  
-  onCommand(username, message) {
-      if (check != 'disabled') {
-        // get the player name in the second word of the message
+onCommand(username, message) {
+  if (check != 'disabled') {
+    if (username == 'Axth' || username == 'oTod' || username == 'SpookyKath' || username == 'Udderly_Cool' || username == 'hitlast' || username == 'SpookyHitlast') {
+      // get the player name in the second word of the message
       const player = message.split(' ')[1]
       const profile = message.split(' ')[2]
       // get the player's stats
       HypAPI.getSkyblockMember(player).then((data) => {
           // console log the data
           console.log(data)
-          // send the amount of fairy souls
-          this.send(`/gc ${data.map}`)
+          this.send(`/gc ${data.fairySouls}`)
       }).catch((err) => {
         this.send(`/gc ${player} dose not exist! - ${makeid(10)}`)
         console.log(`error was caused by ${username}`)
         console.log(err)
       })
     }
-    else {
-      this.send(`/gc This command is disabled! - ${makeid(10)}`)
+    else { 
+      this.send(`/gc You do not have access to this command! - ${makeid(10)}`)
     }
   }
-}
+  else {
+    this.send(`/gc This command is disabled! - ${makeid(10)}`)
+  }
+}}
 
 module.exports = sbCommand
 
