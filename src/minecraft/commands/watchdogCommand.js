cd2f6af0101aaba40b91483bc8e2ae274ef24578
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-14 19:08:44 +0800

    Create watchdogCommand.js

---
+++ src/minecraft/commands/watchdogCommand.js
@@ -0,0 +1,44 @@
+
+const MinecraftCommand = require('../../contracts/MinecraftCommand')
+const HypixelAPIReborn = require('hypixel-api-reborn')
+const HypAPI = new HypixelAPIReborn.Client('cc4cde34-734e-4b33-9bc6-51c65006ccff')
+const check = 'enabled'
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+class watchdogCommand extends MinecraftCommand {
+  constructor(minecraft) {
+    super(minecraft)
+
+    this.name = 'watchdog'
+    this.aliases = ['wd']
+    this.description = 'Shows watchdog stats'
+  }
+
+ 
+onCommand(username, message) {
+    if (check != 'disabled') {
+        HypAPI.getWatchdogStats().then((data) => {
+            this.send(`/gc Watchdog Stats: ${data.byWatchdogTotal} Staff Stats: ${data.byStaffTotal} - ${makeid(10)}`)
+        }).catch((error) => {
+          this.send(`/gc ${player} does not exist! - ${makeid(10)}`)
+          console.log(`error was caused by ${username}`)
+          console.log(error)
+        })
+  } else {
+    this.send(`/gc This command is disabled! - ${makeid(10)}`)
+  }
+}}
+
+module.exports = watchdogCommand
+
+
