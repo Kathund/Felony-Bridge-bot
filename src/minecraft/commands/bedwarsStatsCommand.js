4394a2826f076ecc215ac5c2a38b2a5f598bcf04
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-15 09:18:14 +0800

    Update bedwarsStatsCommand.js

--- src/minecraft/commands/bedwarsStatsCommand.js
+++ src/minecraft/commands/bedwarsStatsCommand.js
@@ -31,7 +31,7 @@ class BedwarsCommand extends MinecraftCommand {
       // get the player's stats
       HypAPI.getPlayer(player).then((data) => {
           // check the ammount of splits in the message
-          if(message.split(' ').length != 2) {
+          if(message.split(' ').length == 3) {
             const mode = message.split(' ')[2]
             if(mode == 'solo' ||
               mode == 'doubles' ||
@@ -41,8 +41,32 @@ class BedwarsCommand extends MinecraftCommand {
               mode == 'castle') {
                 this.send(`/gc Info for ${player} in mode ${mode} - FKDR: ${data.stats.bedwars[mode].finalKDRatio}, Winstreak: ${data.stats.bedwars[mode].winstreak}, Wins: ${data.stats.bedwars[mode].wins}, WLR ${data.stats.bedwars[mode].WLRatio}, BLR ${data.stats.bedwars[mode].beds.BLRatio}, Finals: ${data.stats.bedwars[mode].finalKills}, Beds: ${data.stats.bedwars[mode].beds.broken} - ${makeid(10)}`)
             }
+            else if (mode == 'overall') {
+              this.send(`/gc Info for ${player} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
+            }
+            else if (mode == 'dream') {
+              this.send(`/gc Invalid type! Valid Types: [voidless, lucky, armed, rush, ultimate] - ${makeid(10)}`)
+            }
             else {
-              this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle] - ${makeid(10)}`)
+              this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle, overall, dream] - ${makeid(10)}`)
+            }
+          }
+          else if (message.split(' ').length == 5) {
+            const mode = message.split(' ')[2]
+            const type = message.split(' ')[3]
+            const gamemode = message.split(' ')[4]
+            if (mode == 'dream') {
+              if (type == 'voidless' || type == 'lucky' || type == 'armed' || type == 'rush' || type == 'ultimate') {
+                if (gamemode == 'doubles' || gamemode == 'fours') {
+                  this.send(`/gc Info for ${player} in mode ${mode} ${type} ${gamemode} - FKDR: ${data.stats.bedwars.dream[type][gamemode].finalKDRatio}, Winstreak: ${data.stats.bedwars.dream[type][gamemode].winstreak}, Wins: ${data.stats.bedwars.dream[type][gamemode].wins}, WLR ${data.stats.bedwars.dream[type][gamemode].WLRatio}, BLR ${data.stats.bedwars.dream[type][gamemode].beds.BLRatio}, Finals: ${data.stats.bedwars.dream[type][gamemode].finalKills}, Beds: ${data.stats.bedwars.dream[type][gamemode].beds.broken} - ${makeid(10)}`)
+                } else {
+                  this.send(`/gc Invalid gamemode! Valid Gamemodes: [doubles, fours] - ${makeid(10)}`)
+                }
+              } else {
+                this.send(`/gc Invalid type! Valid Types: [voidless, lucky, armed, rush, ultimate] - ${makeid(10)}`)
+              }
+            } else {
+              this.send(`/gc Invalid mode! Valid Modes: [solo, double, threes, fours, 4v4, castle, overall, dream] - ${makeid(10)}`)
             }
           }
           else {
