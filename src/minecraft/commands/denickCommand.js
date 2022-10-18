453efafc4a188f6c8d8a0736fd8fb22d6ed9a074
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-19 07:41:41 +0800

    Update denickCommand.js

--- src/minecraft/commands/denickCommand.js
+++ src/minecraft/commands/denickCommand.js
@@ -46,22 +46,28 @@ class DenickCommand extends MinecraftCommand {
     if (check != 'disabled') {
         // get the player name in the second word of the message
         const player = message.split(' ')[1]
-        fetch(`http://api.antisniper.net/denick?key=976e7ac3-b818-4442-ae92-c452dfa8b8f1&nick=${player}`).then((res) => {
-          res.json().then((aaa) => {
-            if (aaa.player == null) {
+        if (player == 'help') {
+            this.send(`/gc Usage: !denick <player> - ${makeid(10)}`)
+        }
+        else {
+          fetch(`http://api.antisniper.net/denick?key=976e7ac3-b818-4442-ae92-c452dfa8b8f1&nick=${player}`).then((res) => {
+            res.json().then((aaa) => {
+              if (aaa.player == null) {
+                this.send(`/gc ${player} is not in the database! - ${makeid(10)}`)
+              }
+              const a = (aaa.player.ign)
+              HypAPI.getPlayer(a).then((data) => {
+                this.send(`/gc Denicked - ${player} is ${a} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
+            }).catch(err => {
+              console.log(`error was caused by ${username}`)
+              console.log(err)
               this.send(`/gc ${player} is not in the database! - ${makeid(10)}`)
-            }
-            const a = (aaa.player.ign)
-            HypAPI.getPlayer(a).then((data) => {
-              this.send(`/gc Denicked - ${player} is ${a} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
-          }).catch(err => {
-            console.log(`error was caused by ${username}`)
-            console.log(err)
-            this.send(`/gc ${player} is not in the database! - ${makeid(10)}`)
-          })
+            })
+            }).catch(err => console.log(err));
           }).catch(err => console.log(err));
-        }).catch(err => console.log(err));
-      } else {
+        }
+      }
+      else {
         this.send(`/gc This command is disabled! - ${makeid(10)}`)
       }
   }
