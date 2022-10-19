c2de7c4c3822f425655b5a5d7b1421613ede1ffa
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-20 07:40:26 +0800

    Update denickCommand.js

--- src/minecraft/commands/denickCommand.js
+++ src/minecraft/commands/denickCommand.js
@@ -30,8 +30,8 @@ class DenickCommand extends MinecraftCommand {
  
   onCommand(username, message) {
     if (check != 'disabled') {
-        // get the player name in the second word of the message
-        const player = message.split(' ')[1]
+      const subcommand = message.split(' ')[2]
+      const player = message.split(' ')[1]
         if (player == 'help') {
             this.send(`/gc Usage: !denick <player> - ${makeid(10)}`)
         }
@@ -39,16 +39,23 @@ class DenickCommand extends MinecraftCommand {
           fetch(`http://api.antisniper.net/denick?key=976e7ac3-b818-4442-ae92-c452dfa8b8f1&nick=${player}`).then((res) => {
             res.json().then((aaa) => {
               if (aaa.player == null) {
-                this.send(`/gc ${player} is not in the database! - ${makeid(10)}`)
+                this.send(`/gc ${player} is not in the database! TRY AGAIN in 5 or so mins - ${makeid(10)}`)
               }
               const a = (aaa.player.ign)
-              HypAPI.getPlayer(a).then((data) => {
-                this.send(`/gc Denicked - ${player} is ${a} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
-            }).catch(err => {
-              console.log(`error was caused by ${username}`)
-              console.log(err)
-              this.send(`/gc ${player} is not in the database! - ${makeid(10)}`)
-            })
+              if (subcommand == 'duels') {
+                HypAPI.getPlayer(a).then((data) => {
+                  this.send(`/gc Denicked - ${player} is ${a} - Division: ${data.stats.duels.division} Kills: ${data.stats.duels.kills} Wins: ${data.stats.duels.wins} KD: ${data.stats.duels.KDRatio} WLR: ${data.stats.duels.WLRatio} - ${makeid(10)}`)
+                })
+              }
+              else {
+                HypAPI.getPlayer(a).then((data) => {
+                  this.send(`/gc Denicked - ${player} is ${a} - Star: ${data.stats.bedwars.level}, FKDR: ${data.stats.bedwars.finalKDRatio}, Winstreak: ${data.stats.bedwars.winstreak}, Wins: ${data.stats.bedwars.wins}, WLR ${data.stats.bedwars.WLRatio}, BLR ${data.stats.bedwars.beds.BLRatio}, Finals: ${data.stats.bedwars.finalKills}, Beds: ${data.stats.bedwars.beds.broken} - ${makeid(10)}`)
+                }).catch(err => {
+                  console.log(`error was caused by ${username}`)
+                  console.log(err)
+                  this.send(`/gc ${player} is not in the database! - ${makeid(10)}`)
+                })
+              }
             }).catch(err => console.log(err));
           }).catch(err => console.log(err));
         }
