f60dee59f923110dc02e30fd7f44172058e0b32b
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-19 10:06:48 +0800

    Update guildCommand.js

--- src/minecraft/commands/guildCommand.js
+++ src/minecraft/commands/guildCommand.js
@@ -30,14 +30,19 @@ onCommand(username, message) {
         // get the guild name in the second word of the message
         const guild = message.split(' ')[1]
         // get the guild's stats
-        HypAPI.getGuild('name', guild).then((data) => {
-            // console.log(data)
-            this.send(`/gc Guild info for ${data.name} - Tag: ${data.tag} Members: ${data.members.length} Level: ${data.level} Achievements: Online Players: ${data.achievements.onlinePlayers} Winners: ${data.achievements.winners} - ${makeid(10)}`)
-        }).catch((error) => {
-          this.send(`/gc ${guild} does not exist! - ${makeid(10)}`)
-          console.log(`error was caused by ${username}`)
-          console.log(error)
-        })
+        if (guild == 'help') {
+          this.send(`/gc Usage: !guild <guild name> - ${makeid(10)}`)
+        }
+        else {
+          HypAPI.getGuild('name', guild).then((data) => {
+              // console.log(data)
+              this.send(`/gc Guild info for ${data.name} - Tag: ${data.tag} Members: ${data.members.length} Level: ${data.level} Achievements: Online Players: ${data.achievements.onlinePlayers} Winners: ${data.achievements.winners} - ${makeid(10)}`)
+          }).catch((error) => {
+            this.send(`/gc ${guild} does not exist! - ${makeid(10)}`)
+            console.log(`error was caused by ${username}`)
+            console.log(error)
+          })
+        }
   } else {
     this.send(`/gc This command is disabled! - ${makeid(10)}`)
   }
