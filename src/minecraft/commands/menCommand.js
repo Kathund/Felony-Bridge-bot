c337255ad7582f6c643cd940a878250902a9930f
Kath <55346310+Kathund@users.noreply.github.com>
2022-10-28 20:26:53 +0800

    Update menCommand.js

--- src/minecraft/commands/menCommand.js
+++ src/minecraft/commands/menCommand.js
@@ -1,59 +1,66 @@
+const { getLatestProfile } = require('../../../API/functions/getLatestProfile');
+const { addNotation, addCommas} = require('../../contracts/helperFunctions')
 const MinecraftCommand = require('../../contracts/MinecraftCommand')
-const HypixelAPIReborn = require('hypixel-api-reborn')
-const HypAPI = require('../../Hypixel.js')
-const KEY = '52fda931-396a-407a-bcb8-958f93e8449a'
-const check = 'enabled'
-const fetch = (...args) => import('node-fetch').then(({
-	default: fetch
-}) => fetch(...args)).catch(err => console.log(err));
+const { getNetworth, getPrices } = require('skyhelper-networth');
+const getTalismans = require('../../../API/stats/talismans');
+const getDungeons = require('../../../API/stats/dungeons');
+const getSkills = require('../../../API/stats/skills');
+const getSlayer = require('../../../API/stats/slayer');
+const getWeight = require('../../../API/stats/weight');
 
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
-      charactersLength));
-   }
-   return result;
-}
+let prices;
+getPrices().then((data) => { 
+    prices = data
+})
 
-function Formatter(num, digits) {
-    const lookup = [
-      { value: 1, symbol: "" },
-      { value: 1e3, symbol: "k" },
-      { value: 1e6, symbol: "m" },
-      { value: 1e9, symbol: "b" },
-      { value: 1e12, symbol: "t" },
-      { value: 1e15, symbol: "q" },
-      { value: 1e18, symbol: "qi" }
-    ];
-    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    var item = lookup.slice().reverse().find(function(item) {
-      return num >= item.value;
-    });
-    return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-  }
+setInterval(async () => {
+  prices = await getPrices();
+}, 1000 * 60 * 5); 
 
-class men extends MinecraftCommand {
+class skyblockCommand extends MinecraftCommand {
   constructor(minecraft) {
     super(minecraft)
 
-    this.name = 'men'
-    this.aliases = ['mens']
-    this.description = 'Look up your men'
+    this.name = 'skyblock'
+    this.aliases = ['stats', 'sb']
+    this.description = "Skyblock Stats of specified user."
+    this.options = ['name']
+    this.optionsDescription = ['Minecraft Username']
   }
- 
-onCommand(username, message) {
-  if (check != 'disabled') {
-    
-  }
-  else {
-      this.send(`/gc This command is disabled! - ${makeid(10)}`)
+
+  async onCommand(username, message) {
+    try {
+      let arg = this.getArgs(message)
+      if (arg[0]) username = arg[0]
+      const data = await getLatestProfile(username)
+      username = data.profileData?.game_mode ? `♲ ${username}` : username
+
+      if (!prices) return this.send(`/gc ${username} Prices are still loading, please try again in a few seconds.`)
+      if (data.status == 404) return this.send('/gc There is no player with the given UUID or name or the player has no Skyblock profiles')
+      
+      const [skills, slayer, networth, weight, dungeons, talismans] = await Promise.all([
+        getSkills(data.player, data.profile),
+        getSlayer(data.profile),
+        getNetworth(data.profile, data.profileData?.banking?.balance || 0, { prices }),
+        getWeight(data.profile, data.uuid),
+        getDungeons(data.player, data.profile),
+        getTalismans(data.profile),
+      ])
+      let common = talismans.common?.length, uncommon = talismans.uncommon?.length, rare = talismans.rare?.length, epic = talismans.epic?.length, legendary = talismans.legendary?.length, mythic = talismans.mythic?.length, special = talismans.special?.length, verySpecial = talismans.very?.length, recombobulated = 0, enrichment = 0
+      const talismanCount = common + uncommon + rare + epic + legendary + mythic + special + verySpecial
+      for (const rarity of Object.keys(talismans)) {
+        if (rarity == "talismanBagUpgrades" || rarity == "currentReforge" || rarity == "unlockedReforges" || rarity == "tuningsSlots" || rarity == "tunings") continue
+        for (const talisman of talismans[rarity]) {
+          if (talisman.recombobulated) recombobulated ++
+          if (talisman.enrichment) enrichment ++
+        }
+      }
+      this.send(`/gc ${username}'s Senither Weight » ${Math.round((weight.weight.senither.total) * 100) / 100} | Lily Weight » ${Math.round(weight.weight.lily.total * 100) / 100} | Skill Average » ${Math.round(((skills.farming.level + skills.mining.level + skills.combat.level + skills.foraging.level + skills.fishing.level + skills.enchanting.level + skills.alchemy.level + skills.taming.level + skills.carpentry.level) / 9 )* 100) / 100} | Slayer » ${addCommas(slayer?.[Object.keys(slayer)[0]].xp + slayer?.[Object.keys(slayer)[1]].xp + slayer?.[Object.keys(slayer)[2]].xp + slayer?.[Object.keys(slayer)[3]].xp + slayer?.[Object.keys(slayer)[4]].xp)} | ${slayer?.[Object.keys(slayer)[0]].level} ${slayer?.[Object.keys(slayer)[1]].level} ${slayer?.[Object.keys(slayer)[2]].level} ${slayer?.[Object.keys(slayer)[3]].level} ${slayer?.[Object.keys(slayer)[4]].level} | Catacombs » ${dungeons.catacombs.skill.level} | Class Average » ${((dungeons.classes.healer.level + dungeons.classes.mage.level + dungeons.classes.berserk.level + dungeons.classes.archer.level + dungeons.classes.tank.level) / 5)} | Networth » ${addNotation("oneLetters", networth.networth) ?? 0} | Accessories » ${talismanCount ?? 0} | Recombobulated » ${recombobulated} | Enriched » ${enrichment}`)
+    } catch (error) {
+      console.log(error)
+      this.send('/gc There is no player with the given UUID or name or the player has no Skyblock profiles')
     }
   }
 }
 
-module.exports = men
-
-
+module.exports = skyblockCommand
