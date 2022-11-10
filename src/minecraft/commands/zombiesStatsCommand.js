3cf5748cd85196efe1903bb8f50ef4c2c740e8aa
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-10 17:04:48 +0800

    Update zombiesStatsCommand.js

--- src/minecraft/commands/zombiesStatsCommand.js
+++ src/minecraft/commands/zombiesStatsCommand.js
@@ -18,7 +18,7 @@ class ZombiesCommand extends minecraftCommand {
             if (msg[0]) username = msg[0];
             const player = await hypixel.getPlayer(username);
             var stats = player.stats.arcade.zombies
-            this.send(`/gc [${player.rank} ${player.nickname}]: Wins: ${stats.overall.wins} | Kills: ${stats.overall.zombieKills} | Deaths: ${stats.overall.deaths} | Shots: ${stats.bulletsShot} Hits: ${stats.bulletsHit} Accuracy: ${stats.gunAccuracy} Headshot Accuracy ${stats.headshotAccuracy}`);
+            this.send(`/gc [${player.rank}] ${player.nickname}: Wins: ${stats.overall.wins} | Kills: ${stats.overall.zombieKills} | Deaths: ${stats.overall.deaths} | Shots: ${stats.bulletsShot} Hits: ${stats.bulletsHit} Accuracy: ${stats.gunAccuracy} Headshot Accuracy ${stats.headshotAccuracy}`);
         } catch (error) {
             this.send(
                 "There is no player with the given UUID or name or player has never joined Hypixel."
