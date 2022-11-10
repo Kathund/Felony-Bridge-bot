f4055050e6bd9a89a205cab51194b315abd33b5b
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-10 17:00:31 +0800

    Update playerCommand.js

--- src/minecraft/commands/playerCommand.js
+++ src/minecraft/commands/playerCommand.js
@@ -19,7 +19,7 @@ class PlayerCommand extends minecraftCommand {
             if (msg[0]) username = msg[0];
             const player = await hypixel.getPlayer(username);
             const guild = await hypixel.getGuild('player', username)
-            this.send(`/gc [${player.rank} ${player.nickname}]: Level: ${player.level} | Karma ${addNotation("oneLetters", player.karma)} | Achievement Points ${addNotation("oneLetters", player.achievementPoints)} | Guild: ${guild.name}`);
+            this.send(`/gc [${player.rank}] ${player.nickname}: Level: ${player.level} | Karma ${addNotation("oneLetters", player.karma)} | Achievement Points ${addNotation("oneLetters", player.achievementPoints)} | Guild: ${guild.name}`);
         } catch (error) {
             this.send(
                 "There is no player with the given UUID or name or player has never joined Hypixel."
