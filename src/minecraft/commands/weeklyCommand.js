83bc89f0f7abd73895d8b9634d54b461419c7a6e
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-12 00:02:04 +0800

    Update weeklyCommand.js

--- src/minecraft/commands/weeklyCommand.js
+++ src/minecraft/commands/weeklyCommand.js
@@ -1,6 +1,7 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const { getUUID } = require("../../contracts/API/PlayerDBAPI.js");
 const { getStats } = require("../../contracts/helperFunctions.js");
+const config = require("../../../config.json")
 const fetch = (...args) =>
   import("node-fetch")
     .then(({ default: fetch }) => fetch(...args))
@@ -24,7 +25,7 @@ class WeeklyStatsCommand extends minecraftCommand {
     if (["bw", "bedwars", "bedwar", "bws", "sw", "skywars", "skywar", "sws", "duels", "duel", "d"].includes(args[0])) {
       mode = args[0];
       if (args[1]) player = args[1];
-    } 
+    }
     if (["bw", "bedwars", "bedwar", "bws", "sw", "skywars", "skywar", "sws", "duels", "duel", "d"].includes(args[1])) {
       mode = args[1];
       player = args[0];
@@ -37,14 +38,12 @@ class WeeklyStatsCommand extends minecraftCommand {
     } catch (error) {
       if (error.response?.data?.error == "Player not in database") {
         this.send(
-          `/gc ${
-            player == username ? "You are" : `${player} is`
-          } not in the database. ${
-            player == username ? "You are" : `${player} is`
+          `/gc ${player == username ? "You are" : `${player} is`
+          } not in the database. ${player == username ? "You are" : `${player} is`
           } being added to the database..`
         );
 
-        fetch(`https://api.pixelic.de/v1/player/register?uuid=${uuid}`, {
+        fetch(`https://api.pixelic.de/v1/player/register?key=${config.api.pixelKey}&uuid=${uuid}`, {
           method: "POST",
         }).then((res) => {
           if (res.status == 201) {
