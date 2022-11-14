9c8b6e2c28906f85fc51ac5c1a301abaae3c2ea0
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-14 09:09:25 +0800

    Update gexpRankCheckCommand.js

--- src/minecraft/commands/gexpRankCheckCommand.js
+++ src/minecraft/commands/gexpRankCheckCommand.js
@@ -1,7 +1,7 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
-const config = require("../../../config.example.json")
+const config = require("../../../config.json")
 
 class GEXPRankCommand extends minecraftCommand {
     constructor(minecraft) {
@@ -21,23 +21,33 @@ class GEXPRankCommand extends minecraftCommand {
                 if (arg[0]) {
                     const ign = arg[0];
                     const guild = await hypixel.getGuild('player', ign);
-                    const weekGEXP = guild.me.weeklyExperience
-                    if (weekGEXP > config.minecraft.guild.ranks.guards) {
-                        this.send(`/gc ${ign} is now Guards`)
-                        await delay(1000)
-                        this.minecraft.bot.chat(`/g setrank ${ign} Guards`)
-                    } else if (weekGEXP > config.minecraft.guild.ranks.thieves) {
-                        this.send(`/gc ${ign} is now Thieves`)
-                        await delay(1000)
-                        this.minecraft.bot.chat(`/g setrank ${ign} Thieves`)
-                    } else if (weekGEXP > config.minecraft.guild.ranks.prisoners) {
-                        this.send(`/gc ${ign} is now Prisoners`)
-                        await delay(1000)
-                        this.minecraft.bot.chat(`/g setrank ${ign} Prisoners`)
+                    if (guild.me.rank == "Police" || guild.me.rank == "Wardens" || guild.me.rank == "Guild Master") {
+                        this.send(`/gc ${ign} is a staff member`)
                     } else {
-                        this.send(`/go ${ign} dose not have the 50k gxp`)
-                        await delay(1000)
-                        this.send(`/g kick ${ign} Inactive - If you wish to come back do /g join felony and if you have the reqs it will accept or apply in the discord - gg/felony`)
+                        const weekGEXP = guild.me.weeklyExperience
+                        if (weekGEXP > config.minecraft.guild.ranks.guards) {
+                            this.send(`/gc ${ign} is now Guards`)
+                            await delay(1000)
+                            this.minecraft.bot.chat(`/g setrank ${ign} Guards`)
+                        } else if (weekGEXP > config.minecraft.guild.ranks.thieves) {
+                            this.send(`/gc ${ign} is now Thieves`)
+                            await delay(1000)
+                            this.minecraft.bot.chat(`/g setrank ${ign} Thieves`)
+                        } else if (weekGEXP > config.minecraft.guild.ranks.prisoners) {
+                            this.send(`/gc ${ign} is now Prisoners`)
+                            await delay(1000)
+                            this.minecraft.bot.chat(`/g setrank ${ign} Prisoners`)
+                        } else {
+                            this.send(`/go ${ign} dose not have the 50k gxp`)
+                            await delay(1000)
+                            if (config.minecraft.guild.kicking == false) {
+                                this.send(`/oc Kicking is disabled`)
+                                await delay(1000)
+                                this.minecraft.bot.chat(`/g setrank ${ign} Prisoners`)
+                            } else {
+                                this.send(`/g kick ${ign} Inactive - If you wish to come back do /g join felony and if you have the reqs it will accept or apply in the discord - gg/felony`)
+                            }
+                        }
                     }
                 } else {
                     this.send(`/gc You need to specify a player`)
