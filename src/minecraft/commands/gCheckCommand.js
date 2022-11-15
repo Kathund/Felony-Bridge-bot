14f78a2e0de409afb40ff26c7437558fcc70bad9
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-15 11:19:47 +0800

    Update gCheckCommand.js

--- src/minecraft/commands/gCheckCommand.js
+++ src/minecraft/commands/gCheckCommand.js
@@ -1,6 +1,3 @@
-const fetch = (...args) => import('node-fetch').then(({
-  default: fetch
-}) => fetch(...args)).catch(err => console.log(err));
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
@@ -20,23 +17,56 @@ class GCheckCommand extends minecraftCommand {
     try {
       // const check = await hypixel.getGuild(`player`, username)
       // if (check.me.rank == "Police" || check.me.rank == "Wardens" || check.me.rank == "Guild Master") {
-      if (username == 'Udderly_cool') {
-        const msg = this.getArgs(message);
-        if (msg[0]) username = msg[0];
-        const guild = await hypixel.getGuild('name', config.minecraft.guild.name);
-        var a = guild.members;
+      if (username == 'Udderly_cool' || username == 'Join_FELONY') {
+        const check = await hypixel.getGuild('name', config.minecraft.guild.name);
+        var members = check.members;
 
         var guildMembers = [];
 
-        for (const member in a) {
-            guildMembers.push(a[member].uuid)
+        for (const member in members) {
+          guildMembers.push(members[member].uuid)
         }
-        var amount = guildMembers.length;
-        console.log(amount)
-        console.log(guildMembers[0])
-        var i = guildMembers[1]
-        var player = await hypixel.getPlayer(i)
-        this.send(`/gc ${player.nickname}`)
+        var f = guildMembers.entries();
+        let num = 0
+        this.send(`/oc Checking ${guildMembers.length} members`)
+        await delay(2000)
+        for (let x of f) {
+          var i = guildMembers[num]
+          var player = await hypixel.getPlayer(i)
+          console.log(`${player.nickname} = ${guildMembers[num]} = ${num}`)
+          var guild = await hypixel.getGuild('player', player.nickname)
+          if (guild.me.rank == "Police" || guild.me.rank == "Wardens" || guild.me.rank == "Guild Master") {
+            console.log(`${player.nickname} is a staff member`)
+          } else {
+            const weekGEXP = guild.me.weeklyExperience
+            if (weekGEXP > config.minecraft.guild.ranks.guards) {
+              this.send(`/go ${player.nickname} is now Guards`)
+              await delay(5000)
+              this.minecraft.bot.chat(`/g setrank ${player.nickname} Guards`)
+            } else if (weekGEXP > config.minecraft.guild.ranks.thieves) {
+              this.send(`/go ${player.nickname} is now Thieves`)
+              await delay(5000)
+              this.minecraft.bot.chat(`/g setrank ${player.nickname} Thieves`)
+            } else if (weekGEXP > config.minecraft.guild.ranks.prisoners) {
+              this.send(`/go ${player.nickname} is now Prisoners`)
+              await delay(5000)
+              this.minecraft.bot.chat(`/g setrank ${player.nickname} Prisoners`)
+            } else {
+              this.send(`/go ${player.nickname} dose not have the 50k gxp`)
+              await delay(5000)
+              if (config.minecraft.guild.kicking == false) {
+                this.send(`/oc Kicking is disabled`)
+                await delay(5000)
+                this.minecraft.bot.chat(`/g setrank ${player.nickname} Prisoners`)
+              } else {
+                this.send(`/g kick ${player.nickname} Inactive - If you wish to come back do /g join felony and if you have the reqs it will accept or apply in the discord - gg/felony`)
+              }
+            }
+          }
+          num = num + 1
+          x = x + 1
+        }
+        this.send(`/oc Done`)
       } else {
         // this.send(`/gc This is a staff only command`)
         this.send(`/gc Kaths making this command`)
