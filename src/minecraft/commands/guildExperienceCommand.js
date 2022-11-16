c7ac97be8959e9b246fd65c52c78e480faca0239
kathund <jacobbray2007@gmail.com>
2022-11-16 14:03:24 +0800

    Update guildExperienceCommand.js

--- src/minecraft/commands/guildExperienceCommand.js
+++ src/minecraft/commands/guildExperienceCommand.js
@@ -15,34 +15,20 @@ class GuildEXPCommand extends minecraftCommand {
 
   async onCommand(username, message) {
     try {
+      username = arg[0]
       const arg = this.getArgs(message);
-      if (arg[0]) {
-        username = arg[0]
-        let guild = await hypixel.getGuild('player', username)
-        let rawGexp = guild.me.weeklyExperience
-        let gexp = addCommas(rawGexp)
-        let rank = 'a'
-        if (rawGexp >= config.minecraft.guild.ranks.guards) rank = 'guards'
-        if (rawGexp >= config.minecraft.guild.ranks.thieves) rank = 'thieves'
-        if (rawGexp >= config.minecraft.guild.ranks.prisoners) rank = 'prisoners'
-        if (rank == 'a') {
-          this.send(`/gc ${username}'s GEXP is ${gexp} - They don't have the requirements to stay in ${config.minecraft.guild.name}`)
-        } else {
-          this.send(`/gc ${username}'s GEXP is ${gexp} - They have the requirements for ${rank}`)
-        }
+      if (arg[0]) username = arg[0]
+      let guild = await hypixel.getGuil('player', username)
+      let rawGexp = guild.me.weeklyExperience
+      let gexp = addCommas(rawGexp)
+      let rank = 'a'
+      if (rawGexp >= config.minecraft.guild.ranks.guards) rank = 'guards'
+      if (rawGexp >= config.minecraft.guild.ranks.thieves) rank = 'thieves'
+      if (rawGexp >= config.minecraft.guild.ranks.prisoners) rank = 'prisoners'
+      if (rank == 'a') {
+        this.send(`/gc ${username}'s GEXP is ${gexp} - They don't have the requirements to stay in ${config.minecraft.guild.name}`)
       } else {
-        let guild = await hypixel.getGuil('player', username)
-        let rawGexp = guild.me.weeklyExperience
-        let gexp = addCommas(rawGexp)
-        let rank = 'a'
-        if (rawGexp >= config.minecraft.guild.ranks.guards) rank = 'guards'
-        if (rawGexp >= config.minecraft.guild.ranks.thieves) rank = 'thieves'
-        if (rawGexp >= config.minecraft.guild.ranks.prisoners) rank = 'prisoners'
-        if (rank == 'a') {
-          this.send(`/gc ${username}'s GEXP is ${gexp} - They don't have the requirements to stay in ${config.minecraft.guild.name}`)
-        } else {
-          this.send(`/gc ${username}'s GEXP is ${gexp} - They have the requirements for ${rank}`)
-        }
+        this.send(`/gc ${username}'s GEXP is ${gexp} - They have the requirements for ${rank}`)
       }
     } catch (error) {
       console.log(error);
