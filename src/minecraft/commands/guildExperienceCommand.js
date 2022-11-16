56578d11719f3aebb168bc0cf6fa718ea78562fa
kathund <jacobbray2007@gmail.com>
2022-11-16 11:55:49 +0800

    Update guildExperienceCommand.js

--- src/minecraft/commands/guildExperienceCommand.js
+++ src/minecraft/commands/guildExperienceCommand.js
@@ -1,49 +1,54 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
-const config = require("../../../config.json");
 const { addCommas } = require("../../contracts/helperFunctions.js");
-const { getUUID } = require("../../contracts/API/PlayerDBAPI.js");
+const config = require("../../../config.json")
 
-class GuildExperienceCommand extends minecraftCommand {
-  constructor(minecraft) {
-    super(minecraft);
+class GEXPRankCommand extends minecraftCommand {
+    constructor(minecraft) {
+        super(minecraft);
 
-    this.name = "guildexp";
-    this.aliases = ["gexp"];
-    this.description = "Guilds experience of specified user.";
-    this.options = ["name"];
-  }
-
-  async onCommand(username, message) {
-    const arg = this.getArgs(message);
-    if (arg[0]) username = arg[0];
-
-    try {
-      const [player, guild] = await Promise.all([
-        getUUID(username),
-        hypixel.getGuild("id", config.minecraft.guild.id),
-      ]);
-
-      for (const member of guild.members) {
-        if (guild.members.indexOf(member) == guild.members.length - 1) {
-          return this.send(`/gc ${username} is not in the Guild.`);
-        }
-
-        if (member.uuid != player) continue;
-
-        return this.send(
-          `/gc ${
-            username == arg[0] ? `${arg[0]}'s` : `Your`
-          } Weekly Guild Experience » ${addCommas(member.weeklyExperience)}.`
-        );
-      }
-    } catch (error) {
-      console.log(error);
-      this.send(
-        "There is no player with the given UUID or name or player has never joined Hypixel."
-      );
+        this.name = "grank";
+        this.aliases = [];
+        this.description = "j";
+        this.options = [];
     }
-  }
+
+    async onCommand(username, message) {
+        try {
+          const arg = this.getArgs(message);
+          if (arg[0]) {
+            username = arg[0]
+            var guild = await getGuil('player', username)
+            var rawGexp = guild.me.weeklyExperience
+            var gexp = addCommas(rawGexp)
+            let rank = 'a'
+            if (rawGexp >= config.minecraft.guild.ranks.guards) rank = 'guards'
+            if (rawGexp >= config.minecraft.guild.ranks.thieves) rank = 'thieves'
+            if (rawGexp >= config.minecraft.guild.ranks.prisoners) rank = 'prisoners'
+            if (rank == 'a') {
+              this.send(`/gc ${username}'s GEXP is ${gexp} - They don't have the requirements to stay in ${config.minecraft.guild.name}`)
+            } else {
+              this.send(`/gc ${username}'s GEXP is ${gexp} - They have the requirements for ${rank}`)
+            }
+          } else {
+            var guild = await getGuil('player', username)
+            var rawGexp = guild.me.weeklyExperience
+            var gexp = addCommas(rawGexp)
+            let rank = 'a'
+            if (rawGexp >= config.minecraft.guild.ranks.guards) rank = 'guards'
+            if (rawGexp >= config.minecraft.guild.ranks.thieves) rank = 'thieves'
+            if (rawGexp >= config.minecraft.guild.ranks.prisoners) rank = 'prisoners'
+            if (rank == 'a') {
+              this.send(`/gc ${username}'s GEXP is ${gexp} - They don't have the requirements to stay in ${config.minecraft.guild.name}`)
+            } else {
+              this.send(`/gc ${username}'s GEXP is ${gexp} - They have the requirements for ${rank}`)
+            }
+          }
+        } catch (error) {
+            console.log(error);
+            this.send("/gc Something went wrong..");
+        }
+    }
 }
 
-module.exports = GuildExperienceCommand;
+module.exports = GEXPRankCommand;
