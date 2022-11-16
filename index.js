853b92c19d6f2db97dda20dfc8bdc43069dc4192
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-16 17:30:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ process.on('uncaughtException', function (error) {console.log(error)})
 const webServer = require('./src/web/server.js'); 
 const app = require('./src/Application.js')
 
-process.title = 'Hypixel Discord Chat Bridge | by DuckySoLucky#5181'
+process.title = 'Hypixel Discord Chat Bridge | by Kathund#2004'
 'use strict'; 
 
 app.register().then(() => {
