2cbc079c324f1b534ba52cfa7f7542d7ee1c4aab
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-19 21:23:09 +0800

    Update exampleCommand.js

--- src/minecraft/commands/exampleCommand.js
+++ src/minecraft/commands/exampleCommand.js
@@ -12,15 +12,10 @@ class ExampleCommand extends minecraftCommand {
 
   async onCommand(username, message) {
     try {
-      if (username == 'hitlast') {
-        this.send(`/g kick qSamantha Inactive - If you wish to come back do /g join felony and if you have the reqs it will accept or apply in the discord - gg/felony`)
-      } else {
-        
       console.log(`the username var represents the username of the person running the command - ${username}`)
       console.log(`the message var represents the full message from the user - ${message}`)
       console.log(`use this.send to send a message as the bot`)
       this.send(`/gc Check the console`)
-      }
     } catch (error) {
       console.log(error);
       this.send("/gc Something went wrong..");
