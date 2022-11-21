9aad33a98b98046aefb6a421c75d3c082339a516
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-21 16:08:28 +0800

    Update reloadCommand.js

--- src/discord/commands/reloadCommand.js
+++ src/discord/commands/reloadCommand.js
@@ -10,20 +10,19 @@ module.exports = {
         let hasPerms = false;
         if ((await interaction.guild.members.fetch(interaction.user)).roles.cache.has(config.discord.roles.botDev)) hasPerms = true;
         if (hasPerms) {
-            const name = interaction.options.getString("name");
             bot.chat(`/gc The Bot is restarting!`)
             const loggingEmbed = new EmbedBuilder()
                 .setColor(16733070)
                 .setTitle(`Bot Restarting!`)
-                .setThumbnail(`https://www.mc-heads.net/avatar/Udderly_cool`)
                 .setTimestamp()
                 .setFooter({ text: `by Kathund#2004 | /help [command] for more information` });
             await client.channels.cache.get(`${config.discord.loggingChannel}`).send({ embeds: [loggingEmbed] });
-            hypixel.getPlayer('Kath likes men')
+            // exit the process
             await interaction.followUp({
                 content: "Command has been executed successfully.",
                 ephemeral: true,
             });
+            process.exit(0);
         } else {
             await interaction.followUp({
                 content: "You do not have permission to run this command.",
