5cc443fa52c786f9fd3bc934a669fbdbf2608ca7
kathund <jacobbray2007@gmail.com>
2022-11-22 14:03:17 +0800

    Create pitCommand.js

---
+++ src/minecraft/commands/pitCommand.js
@@ -0,0 +1,42 @@
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
+const fetch = (...args) =>
+    import("node-fetch")
+        .then(({ default: fetch }) => fetch(...args))
+        .catch((err) => console.log(err));
+
+class PitCommand extends minecraftCommand {
+    constructor(minecraft) {
+        super(minecraft);
+
+        this.name = "pit";
+        this.aliases = [];
+        this.description = "pit stats";
+        this.options = [];
+    }
+
+    async onCommand(username, message) {
+        try {
+            const msg = this.getArgs(message);
+            if (msg[0]) username = msg[0]
+            const player = await hypixel.getPlayer(username);
+            this.send(`/gc [${player.rank}] ${player.nickname}: Kills: ${player.stats.pit.kills} | I know this is nothing but this is the only thing in the api lmao`)
+            fetch(`https://api.pixelic.de/v1/player/register?key=${config.api.pixelKey}&uuid=${uuid}`, {
+                method: "POST",
+            }).then((res) => {
+                if (res.status == 201) {
+                    console.log(`/gc Successfully registered ${player} in the database!`);
+                } else if (res.status == 400) {
+                    console.log(`/gc ${player} is already registered in the database!`);
+                } else {
+                    console.log(`/gc An error occured while registering ${player} in the database! Please try again in few seconds.`);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            this.send("/gc Something went wrong..");
+        }
+    }
+}
+
+module.exports = PitCommand;
