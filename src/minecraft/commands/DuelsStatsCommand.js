baee3e088f4ec4d08429ae053102d78e43ec05fc
kathund <jacobbray2007@gmail.com>
2022-11-22 14:22:28 +0800

    Update DuelsStatsCommand.js

--- src/minecraft/commands/DuelsStatsCommand.js
+++ src/minecraft/commands/DuelsStatsCommand.js
@@ -19,6 +19,7 @@ class DuelsStatsCommand extends minecraftCommand {
 
   async onCommand(username, message) {
     try {
+      const player = hypixel.getPlayer(username);
       const duelTypes = [
         "blitz",
         "uhc",
@@ -96,11 +97,11 @@ class DuelsStatsCommand extends minecraftCommand {
         method: "POST",
       }).then((res) => {
         if (res.status == 201) {
-          console.log(`/gc Successfully registered ${player} in the database!`);
+          console.log(`/gc Successfully registered ${player.nickname} in the database!`);
         } else if (res.status == 400) {
-          console.log(`/gc ${player} is already registered in the database!`);
+          console.log(`/gc ${player.nickname} is already registered in the database!`);
         } else {
-          console.log(`/gc An error occured while registering ${player} in the database! Please try again in few seconds.`);
+          console.log(`/gc An error occured while registering ${player.nickanem} in the database! Please try again in few seconds.`);
         }
       });
     } catch (error) {
