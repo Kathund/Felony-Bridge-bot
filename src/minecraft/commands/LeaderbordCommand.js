c458fab4ca0508c9746f3a8786543c5913ca9843
kathund <jacobbray2007@gmail.com>
2022-11-24 12:34:18 +0800

    Update LeaderbordCommand.js

--- src/minecraft/commands/LeaderbordCommand.js
+++ src/minecraft/commands/LeaderbordCommand.js
@@ -23,21 +23,10 @@ class LeaderBoardCommand extends minecraftCommand {
             if (username == "Udderly_cool") {
                 fetch(`https://api.pixelic.de/v1/leaderboard?key=${config.api.pixelKey}&mode=bedwars&timeframe=lifetime&limit=10`).then((res) => {
                     res.json().then(async (data) => {
-                        var player1 = await getUsername(data.overall.wins[0].uuid);
-                        var player2 = await getUsername(data.overall.wins[1].uuid);
-                        var player3 = await getUsername(data.overall.wins[2].uuid);
-                        var player4 = await getUsername(data.overall.wins[3].uuid);
-                        var player5 = await getUsername(data.overall.wins[4].uuid);
-                        var player6 = await getUsername(data.overall.wins[5].uuid);
-                        var player7 = await getUsername(data.overall.wins[6].uuid);
-                        var player8 = await getUsername(data.overall.wins[7].uuid);
-                        var player9 = await getUsername(data.overall.wins[8].uuid);
-                        var player10 = await getUsername(data.overall.wins[9].uuid);
+                        console.log(`#1 ${getUsername(data.overall.wins[0].uuid)} = ${data.overall.wins[0].wins} | #2 ${getUsername(data.overall.wins[1].uuid)} = ${data.overall.wins[1].wins} | #3 ${getUsername(data.overall.wins[2].uuid)} = ${data.overall.wins[2].wins} | #4 ${getUsername(data.overall.wins[3].uuid)} = ${data.overall.wins[3].wins} | #5 ${getUsername(data.overall.wins[4].uuid)} = ${data.overall.wins[4].wins} | #6 ${getUsername(data.overall.wins[5].uuid)} = ${data.overall.wins[5].wins} | #7 ${getUsername(data.overall.wins[6].uuid)} = ${data.overall.wins[6].wins} | #8 ${getUsername(data.overall.wins[7].uuid)} = ${data.overall.wins[7].wins} | #9 ${getUsername(data.overall.wins[8].uuid)} = ${data.overall.wins[8].wins} | #10 ${getUsername(data.overall.wins[9].uuid)} = ${data.overall.wins[9].wins}`);
                     })
                 })
-            } else {
-                this.send("You are not Udderly_cool");
-            }
+            } else {}
         } catch (error) {
             console.log(error);
             this.send("/gc Something went wrong..");
