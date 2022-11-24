df322380656d62f00c44d9ca727a888e57e58174
kathund <jacobbray2007@gmail.com>
2022-11-24 14:16:19 +0800

    Create socialMediaCommand.js

---
+++ src/minecraft/commands/socialMediaCommand.js
@@ -0,0 +1,28 @@
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
+
+class SocialMediaCommand extends minecraftCommand {
+    constructor(minecraft) {
+        super(minecraft);
+
+        this.name = "socialmedia";
+        this.aliases = ['sm'];
+        this.description = "Shows the linked Social Media of a player";
+        this.options = [];
+    }
+
+    async onCommand(username, message) {
+        try {
+            const msg = this.getArgs(message);
+            if (msg[0]) username = msg[0]
+            var player = hypixel.getPlayer(username)
+            this.send(`no`)
+            console.log(player.socialMedia)
+        } catch (error) {
+            console.log(error);
+            this.send("/gc Something went wrong..");
+        }
+    }
+}
+
+module.exports = SocialMediaCommand;
