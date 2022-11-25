037716b722a81f2c86188238ac46e357df1b3913
kathund <jacobbray2007@gmail.com>
2022-11-25 09:15:22 +0800

    Update LeaderbordCommand.js

--- src/minecraft/commands/LeaderbordCommand.js
+++ src/minecraft/commands/LeaderbordCommand.js
@@ -22,24 +22,26 @@ class LeaderBoardCommand extends minecraftCommand {
             if (username == "Udderly_cool") {
                 const msg = this.getArgs(message);
                 // check mode
-                if (msg[0] == ['bw', 'bedwars', 'bedwar', 'bws']) var mode = 'bedwars';
-                if (msg[0] == ['sw', 'skywars', 'skywar', 'sws']) var mode = 'skywars';
-                if (msg[0] == ['duels', 'duel', 'd']) var mode = 'duels';
-                if (msg[0] == ['general', 'gen', 'g']) var mode = 'general';
-                if (msg[1] == ['bw', 'bedwars', 'bedwar', 'bws']) var mode = 'bedwars';
-                if (msg[1] == ['sw', 'skywars', 'skywar', 'sws']) var mode = 'skywars';
-                if (msg[1] == ['duels', 'duel', 'd']) var mode = 'duels';
-                if (msg[1] == ['general', 'gen', 'g']) var mode = 'general';
+                let mode = 'a'
+                if (msg[0] == ['bw', 'bedwars', 'bedwar', 'bws']) mode = 'bedwars';
+                if (msg[0] == ['sw', 'skywars', 'skywar', 'sws']) mode = 'skywars';
+                if (msg[0] == ['duels', 'duel', 'd']) mode = 'duels';
+                if (msg[0] == ['general', 'gen', 'g']) mode = 'general';
+                if (msg[1] == ['bw', 'bedwars', 'bedwar', 'bws']) mode = 'bedwars';
+                if (msg[1] == ['sw', 'skywars', 'skywar', 'sws']) mode = 'skywars';
+                if (msg[1] == ['duels', 'duel', 'd']) mode = 'duels';
+                if (msg[1] == ['general', 'gen', 'g']) mode = 'general';
 
                 // check timeframe
-                if (msg[0] == ['daily', 'day', 'd']) var timeframe = 'daily';
-                if (msg[0] == ['weekly', 'week', 'w']) var timeframe = 'weekly';
-                if (msg[0] == ['monthly', 'month', 'm']) var timeframe = 'monthly';
-                if (msg[0] == ['lifetime', 'alltime', 'all', 'l']) var timeframe = 'lifetime';
-                if (msg[1] == ['daily', 'day', 'd']) var timeframe = 'daily';
-                if (msg[1] == ['weekly', 'week', 'w']) var timeframe = 'weekly';
-                if (msg[1] == ['monthly', 'month', 'm']) var timeframe = 'monthly';
-                if (msg[1] == ['lifetime', 'alltime', 'all', 'l']) var timeframe = 'lifetime';
+                let timeframe = 'a'
+                if (msg[0] == ['daily', 'day', 'd']) timeframe = 'daily';
+                if (msg[0] == ['weekly', 'week', 'w']) timeframe = 'weekly';
+                if (msg[0] == ['monthly', 'month', 'm']) timeframe = 'monthly';
+                if (msg[0] == ['lifetime', 'alltime', 'all', 'l']) timeframe = 'lifetime';
+                if (msg[1] == ['daily', 'day', 'd']) timeframe = 'daily';
+                if (msg[1] == ['weekly', 'week', 'w']) timeframe = 'weekly';
+                if (msg[1] == ['monthly', 'month', 'm']) timeframe = 'monthly';
+                if (msg[1] == ['lifetime', 'alltime', 'all', 'l']) timeframe = 'lifetime';
                 fetch(`https://api.pixelic.de/v1/leaderboard?key=${config.api.pixelKey}&mode=${mode}&timeframe=${timeframe}&limit=10`).then((res) => {
                     res.json().then(async (data) => {
                     })
