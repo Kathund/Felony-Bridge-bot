399c2e8d9a34d33fe08b13e75ceeb1ea651bb0cc
Kath <55346310+Kathund@users.noreply.github.com>
2022-11-26 09:25:41 +0800

    Update bedwarsWinstreakCommand.js

--- src/minecraft/commands/bedwarsWinstreakCommand.js
+++ src/minecraft/commands/bedwarsWinstreakCommand.js
@@ -2,12 +2,10 @@ const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
 const config = require("../../../config.json");
 const fetch = (...args) =>
-    import("node-fetch")
-        .then(({ default: fetch }) => fetch(...args))
-        .catch((err) => console.log(err));
-
-const axios = require("axios");
-
+  import("node-fetch")
+    .then(({ default: fetch }) => fetch(...args))
+    .catch((err) => console.log(err));
+``
 class DenickerCommand extends minecraftCommand {
   constructor(minecraft) {
     super(minecraft);
@@ -23,19 +21,13 @@ class DenickerCommand extends minecraftCommand {
     try {
       const arg = this.getArgs(message);
       if (arg[0]) username = arg[0];
-      const [player, response] = await Promise.all([
-        hypixel.getPlayer(username),
-        axios.get(
-          `${config.api.antiSniperAPI}/winstreak?key=${config.api.antiSniperKey}&name=${username}`
-        ),
-      ]);
-      this.send(
-        `/gc [${player.stats.bedwars.level}✫] ${player.nickname}: Accurrate » ${response.data.player.accurate ? "Yes" : "No"
-        } | Overall » ${response.data.player.data.overall_winstreak} | Solo » ${response.data.player.data.eight_one_winstreak
-        } | Doubles » ${response.data.player.data.eight_two_winstreak} | Trios » ${response.data.player.data.four_three_winstreak
-        } | Fours » ${response.data.player.data.four_four_winstreak} | 4v4  » ${response.data.player.data.two_four_winstreak
-        }`
-      );
+      const player = hypixel.getPlayer(username);
+      fetch(`${config.api.antiSniperAPI}/winstreak?key=${config.api.antiSniperKey}&name=${username}`).then((res) => {
+        res.json().then((data) => {
+          console.log(data)
+          // this.send(`/gc [${player.stats.bedwars.level}✫] ${player.nickname}: Accurrate » ${response.data.player.accurate ? "Yes" : "No"} | Overall » ${response.data.player.data.overall_winstreak} | Solo » ${response.data.player.data.eight_one_winstreak} | Doubles » ${response.data.player.data.eight_two_winstreak} | Trios » ${response.data.player.data.four_three_winstreak} | Fours » ${response.data.player.data.four_four_winstreak} | 4v4  » ${response.data.player.data.two_four_winstreak}`);
+        });
+      });
       fetch(`https://api.pixelic.de/v1/player/register?key=${config.api.pixelKey}&uuid=${player.uuid}`, {
         method: "POST",
       }).then((res) => {
