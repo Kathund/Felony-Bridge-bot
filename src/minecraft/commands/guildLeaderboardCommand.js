a5917838f569e08d48ce240a7a7d93ee136451a2
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-03 09:20:10 +0800

    Fixed eslint erroes
    - 200 eslint errors

--- src/minecraft/commands/guildLeaderboardCommand.js
+++ src/minecraft/commands/guildLeaderboardCommand.js
@@ -1,28 +1,27 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js")
 const config = require("../../../config.json")
-const hypixel = require("../../contracts/API/HypixelRebornAPI.js")
 const fetch = (...args) =>
   import("node-fetch")
     .then(({ default: fetch }) => fetch(...args))
     .catch((err) => console.log(err))
 
 const axios = require("axios")
-async function getUsername(UUID) {
-  const fetch = await axios.get(`https://sessionserver.mojang.com/session/minecraft/profile/${UUID}`)
+async function getUsername(uuid) {
+  const fetch = await axios.get(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`)
   return fetch.data.name
 }
-async function getUUID(Username) {
-  const fetch = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${Username}`)
+async function getUUID(username) {
+  const fetch = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`)
   return fetch.data.id
 }
 
-class guildLeaderboardCommand extends minecraftCommand {
+class GuildLeaderboardCommand extends minecraftCommand {
   constructor(minecraft) {
     super(minecraft)
 
     this.name = "glb"
     this.aliases = ["guildlb", "guildleaderboard"]
-    this.description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    this.description = "Alows you to look up the leaderbaords for guilds"
     this.options = []
   }
 
@@ -32,254 +31,99 @@ class guildLeaderboardCommand extends minecraftCommand {
       //check mode
       var mode
 
-      if (["bw", "bedwars", "bedwar", "bws"].includes(msg[0])) {
-        var mode = "Bedwars"
-      }
-      if (["sw", "skywars", "skywar", "sws"].includes(msg[0])) {
-        var mode = "Skywars"
-      }
-      if (["duels", "duel", "d"].includes(msg[0])) {
-        var mode = "Duels"
-      }
-      if (["general", "gen", "g"].includes(msg[0])) {
-        var mode = "General"
-      }
-      if (["bw", "bedwars", "bedwar", "bws"].includes(msg[1])) {
-        var mode = "Bedwars"
-      }
-      if (["sw", "skywars", "skywar", "sws"].includes(msg[1])) {
-        var mode = "Skywars"
-      }
-      if (["duels", "duel", "d"].includes(msg[1])) {
-        var mode = "Duels"
-      }
-      if (["general", "gen", "g"].includes(msg[1])) {
-        var mode = "General"
-      }
+      if (["bw", "bedwars", "bedwar", "bws"].includes(msg[0])) mode = "Bedwars"
+      if (["sw", "skywars", "skywar", "sws"].includes(msg[0])) mode = "Skywars"
+      if (["duels", "duel", "d"].includes(msg[0])) mode = "Duels"
+      if (["general", "gen", "g"].includes(msg[0])) mode = "General"
+      if (["bw", "bedwars", "bedwar", "bws"].includes(msg[1])) mode = "Bedwars"
+      if (["sw", "skywars", "skywar", "sws"].includes(msg[1])) mode = "Skywars"
+      if (["duels", "duel", "d"].includes(msg[1])) mode = "Duels"
+      if (["general", "gen", "g"].includes(msg[1])) mode = "General"
 
       //check timeframe
       var timeframe
-      if (["daily", "day", "d"].includes(msg[0])) {
-        var timeframe = "Daily"
-      }
-      if (["weekly", "week", "w"].includes(msg[0])) {
-        var timeframe = "Weekly"
-      }
-      if (["monthly", "month", "m"].includes(msg[0])) {
-        var timeframe = "Monthly"
-      }
-      if (["lifetime", "alltime", "all", "l"].includes(msg[0])) {
-        var timeframe = "Lifetime"
-      }
-      if (["daily", "day", "d"].includes(msg[1])) {
-        var timeframe = "Daily"
-      }
-      if (["weekly", "week", "w"].includes(msg[1])) {
-        var timeframe = "Weekly"
-      }
-      if (["monthly", "month", "m"].includes(msg[1])) {
-        var timeframe = "Monthly"
-      }
-      if (["lifetime", "alltime", "all", "l"].includes(msg[1])) {
-        var timeframe = "Lifetime"
-      }
+      if (["daily", "day", "d"].includes(msg[0])) timeframe = "Daily"
+      if (["weekly", "week", "w"].includes(msg[0])) timeframe = "Weekly"
+      if (["monthly", "month", "m"].includes(msg[0])) timeframe = "Monthly"
+      if (["lifetime", "alltime", "all", "l"].includes(msg[0])) timeframe = "Lifetime"
+      if (["daily", "day", "d"].includes(msg[1])) timeframe = "Daily"
+      if (["weekly", "week", "w"].includes(msg[1])) timeframe = "Weekly"
+      if (["monthly", "month", "m"].includes(msg[1])) timeframe = "Monthly"
+      if (["lifetime", "alltime", "all", "l"].includes(msg[1])) timeframe = "Lifetime"
 
       fetch(`https://api.pixelic.de/v1/leaderboard/guild?key=${config.api.pixelKey}&mode=${mode.toLowerCase()}&timeframe=${timeframe.toLowerCase()}&limit=10&uuid=${await getUUID(username)}`).then((res) => {
         res.json().then(async (data) => {
-          var gamemode
-          var gamemodeFormatted
-          var type
-          var typeFormatted
+          var gamemode = null
+          var gamemodeFormatted = null
+          var type = null
+          var typeFormatted = null
           if (mode.toLowerCase() == "bedwars") {
             //check gamemode
-            if (msg[2] == "overall") {
-              var gamemode = "overall"
-              var gamemodeFormatted = "Overall"
-            }
-            if (msg[2] == "solo") {
-              var gamemode = "solo"
-              var gamemodeFormatted = "Solo"
-            }
-            if (msg[2] == "doubles") {
-              var gamemode = "doubles"
-              var gamemodeFormatted = "Doubles"
-            }
-            if (msg[2] == "threes") {
-              var gamemode = "threes"
-              var gamemodeFormatted = "threes"
-            }
-            if (msg[2] == "fours") {
-              var gamemode = "fours"
-              var gamemodeFormatted = "Fours"
-            }
-            if (["four_two", "4v4"].includes(msg[2])) {
-              var gamemode = "four_two"
-              var gamemodeFormatted = "4v4"
-            }
+            if (msg[2] == "overall") gamemode = "overall", gamemodeFormatted = "Overall"
+            if (msg[2] == "solo") gamemode = "solo", gamemodeFormatted = "Solo"
+            if (msg[2] == "doubles") gamemode = "doubles", gamemodeFormatted = "Doubles"
+            if (msg[2] == "threes") gamemode = "threes", gamemodeFormatted = "threes"
+            if (msg[2] == "fours") gamemode = "fours", gamemodeFormatted = "Fours"
+            if (["four_two", "4v4"].includes(msg[2])) gamemode = "four_two", gamemodeFormatted = "4v4"
 
             //check stat type
-            if (msg[3] == "wins") {
-              var type = "wins"
-              var typeFormatted = "Wins"
-            }
-            if (msg[3] == "losses") {
-              var type = "losses"
-              var typeFormatted = "Losses"
-            }
-            if (msg[3] == "wlr") {
-              var type = "wlr"
-              var typeFormatted = "WLR"
-            }
-            if (["finals", "finalkills", "fk", "fks", "fkill", "fkills"].includes(msg[3])) {
-              var type = "finalKills"
-              var typeFormatted = "Finalkills"
-            }
-            if (["finaldeaths", "fd", "fds", "fdeath", "fdeaths"].includes(msg[3])) {
-              var type = "finalDeaths"
-              var typeFormatted = "Finaldeaths"
-            }
-            if (msg[3] == "fkdr") {
-              var type = "fkdr"
-              var typeFormatted = "FKDR"
-            }
-            if (["kills", "k"].includes(msg[3])) {
-              var type = "kills"
-              var typeFormatted = "Kills"
-            }
-            if (["deaths", "d"].includes(msg[3])) {
-              var type = "deaths"
-              var typeFormatted = "Deaths"
-            }
-            if (msg[3] == "kdr") {
-              var type = "kdr"
-              var typeFormatted = "KDR"
-            }
-            if (["bedsbroken", "bb"].includes(msg[3])) {
-              var type = "bedsBroken"
-              var typeFormatted = "BedsBroken"
-            }
-            if (["bedslost", "bl"].includes(msg[3])) {
-              var type = "bedsLost"
-              var typeFormatted = "BedsLost"
-            }
-            if (msg[3] == "bblr") {
-              var type = "bblr"
-              var typeFormatted = "BBLR"
-            }
+            if (msg[3] == "wins") type = "wins", typeFormatted = "Wins"
+            if (msg[3] == "losses") type = "losses", typeFormatted = "Losses"
+            if (msg[3] == "wlr") type = "wlr", typeFormatted = "WLR"
+            if (["finals", "finalkills", "fk", "fks", "fkill", "fkills"].includes(msg[3])) type = "finalKills", typeFormatted = "Finalkills"
+            if (["finaldeaths", "fd", "fds", "fdeath", "fdeaths"].includes(msg[3])) type = "finalDeaths", typeFormatted = "Finaldeaths"
+            if (msg[3] == "fkdr") type = "fkdr", typeFormatted = "FKDR"
+            if (["kills", "k"].includes(msg[3])) type = "kills", typeFormatted = "Kills"
+            if (["deaths", "d"].includes(msg[3])) type = "deaths", typeFormatted = "Deaths"
+            if (msg[3] == "kdr") type = "kdr", typeFormatted = "KDR"
+            if (["bedsbroken", "bb"].includes(msg[3])) type = "bedsBroken", typeFormatted = "BedsBroken"
+            if (["bedslost", "bl"].includes(msg[3])) type = "bedsLost", typeFormatted = "BedsLost"
+            if (msg[3] == "bblr") type = "bblr", typeFormatted = "BBLR"
+
             this.send(
-              `/gc [Guild] ${timeframe} ${mode} ${gamemodeFormatted} ${typeFormatted} » #1 ${await getUsername(data[gamemode][type][0].UUID)}: ${data[gamemode][type][0][type]} | #2 ${await getUsername(data[gamemode][type][1].UUID)}: ${data[gamemode][type][1][type]} | #3 ${await getUsername(data[gamemode][type][2].UUID)}: ${data[gamemode][type][2][type]} | #4 ${await getUsername(data[gamemode][type][3].UUID)}: ${data[gamemode][type][3][type]} | #5 ${await getUsername(data[gamemode][type][4].UUID)}: ${
-                data[gamemode][type][4][type]
+              `/gc [Guild] ${timeframe} ${mode} ${gamemodeFormatted} ${typeFormatted} » #1 ${await getUsername(data[gamemode][type][0].UUID)}: ${data[gamemode][type][0][type]} | #2 ${await getUsername(data[gamemode][type][1].UUID)}: ${data[gamemode][type][1][type]} | #3 ${await getUsername(data[gamemode][type][2].UUID)}: ${data[gamemode][type][2][type]} | #4 ${await getUsername(data[gamemode][type][3].UUID)}: ${data[gamemode][type][3][type]} | #5 ${await getUsername(data[gamemode][type][4].UUID)}: ${data[gamemode][type][4][type]
               } | #6 ${await getUsername(data[gamemode][type][5].UUID)}: ${data[gamemode][type][5][type]} | #7 ${await getUsername(data[gamemode][type][6].UUID)}: ${data[gamemode][type][6][type]} | #8 ${await getUsername(data[gamemode][type][7].UUID)}: ${data[gamemode][type][7][type]} | #9 ${await getUsername(data[gamemode][type][8].UUID)}: ${data[gamemode][type][8][type]} | #10 ${await getUsername(data[gamemode][type][9].UUID)}: ${data[gamemode][type][9][type]}`
             )
           } else if (mode.toLowerCase() == "skywars") {
             //Check gamemode
-            if (msg[2] == "overall") {
-              var gamemode = "overall"
-              var gamemodeFormatted = "Overall"
-            }
-            if (msg[2] == "solo") {
-              var gamemode = "solo"
-              var gamemodeFormatted = "Solo"
-            }
-            if (["teams", "doubles"].includes(msg[2])) {
-              var gamemode = "doubles"
-              var gamemodeFormatted = "Doubles"
-            }
+            if (msg[2] == "overall") gamemode = "overall", gamemodeFormatted = "Overall"
+            if (msg[2] == "solo") gamemode = "solo", gamemodeFormatted = "Solo"
+            if (["teams", "doubles"].includes(msg[2])) gamemode = "doubles", gamemodeFormatted = "Doubles"
 
             //  Check Stat Type
-            if (msg[3] == "wins") {
-              var type = "wins"
-              var typeFormatted = "Wins"
-            }
-            if (msg[3] == "losses") {
-              var type = "losses"
-              var typeFormatted = "Losses"
-            }
-            if (msg[3] == "wlr") {
-              var type = "wlr"
-              var typeFormatted = "WLR"
-            }
-            if (msg[3] == "fkdr") {
-              var type = "fkdr"
-              var typeFormatted = "FKDR"
-            }
-            if (["kills", "k"].includes(msg[3])) {
-              var type = "kills"
-              var typeFormatted = "Kills"
-            }
-            if (["deaths", "d"].includes(msg[3])) {
-              var type = "deaths"
-              var typeFormatted = "Deaths"
-            }
-            if (msg[3] == "kdr") {
-              var type = "kdr"
-              var typeFormatted = "KDR"
-            }
+            if (msg[3] == "wins") type = "wins", typeFormatted = "Wins"
+            if (msg[3] == "losses") type = "losses", typeFormatted = "Losses"
+            if (msg[3] == "wlr") type = "wlr", typeFormatted = "WLR"
+            if (["kills", "k"].includes(msg[3])) type = "kills", typeFormatted = "Kills"
+            if (["deaths", "d"].includes(msg[3])) type = "deaths", typeFormatted = "Deaths"
+            if (msg[3] == "kdr") type = "kdr", typeFormatted = "KDR"
 
             this.send(
-              `/gc [Guild] ${timeframe} ${mode} ${gamemodeFormatted} ${typeFormatted} » #1 ${await getUsername(data[gamemode][type][0].UUID)}: ${data[gamemode][type][0][type]} | #2 ${await getUsername(data[gamemode][type][1].UUID)}: ${data[gamemode][type][1][type]} | #3 ${await getUsername(data[gamemode][type][2].UUID)}: ${data[gamemode][type][2][type]} | #4 ${await getUsername(data[gamemode][type][3].UUID)}: ${data[gamemode][type][3][type]} | #5 ${await getUsername(data[gamemode][type][4].UUID)}: ${
-                data[gamemode][type][4][type]
+              `/gc [Guild] ${timeframe} ${mode} ${gamemodeFormatted} ${typeFormatted} » #1 ${await getUsername(data[gamemode][type][0].UUID)}: ${data[gamemode][type][0][type]} | #2 ${await getUsername(data[gamemode][type][1].UUID)}: ${data[gamemode][type][1][type]} | #3 ${await getUsername(data[gamemode][type][2].UUID)}: ${data[gamemode][type][2][type]} | #4 ${await getUsername(data[gamemode][type][3].UUID)}: ${data[gamemode][type][3][type]} | #5 ${await getUsername(data[gamemode][type][4].UUID)}: ${data[gamemode][type][4][type]
               } | #6 ${await getUsername(data[gamemode][type][5].UUID)}: ${data[gamemode][type][5][type]} | #7 ${await getUsername(data[gamemode][type][6].UUID)}: ${data[gamemode][type][6][type]} | #8 ${await getUsername(data[gamemode][type][7].UUID)}: ${data[gamemode][type][7][type]} | #9 ${await getUsername(data[gamemode][type][8].UUID)}: ${data[gamemode][type][8][type]} | #10 ${await getUsername(data[gamemode][type][9].UUID)}: ${data[gamemode][type][9][type]}`
             )
           } else if (mode.toLowerCase() == "duels") {
-            if (msg[2] == "overall") {
-              var gamemode = "overall"
-              var gamemodeFormatted = "Overall"
-            }
+            if (msg[2] == "overall") gamemode = "overall", gamemodeFormatted = "Overall"
 
             // Check Stat Type
-            if (msg[3] == "wins") {
-              var type = "wins"
-              var typeFormatted = "Wins"
-            }
-            if (msg[3] == "losses") {
-              var type = "losses"
-              var typeFormatted = "Losses"
-            }
-            if (msg[3] == "wlr") {
-              var type = "wlr"
-              var typeFormatted = "WLR"
-            }
-            if (msg[3] == "fkdr") {
-              var type = "fkdr"
-              var typeFormatted = "FKDR"
-            }
-            if (["kills", "k"].includes(msg[3])) {
-              var type = "kills"
-              var typeFormatted = "Kills"
-            }
-            if (["deaths", "d"].includes(msg[3])) {
-              var type = "deaths"
-              var typeFormatted = "Deaths"
-            }
-            if (msg[3] == "kdr") {
-              var type = "kdr"
-              var typeFormatted = "KDR"
-            }
+            if (msg[3] == "wins") type = "wins", typeFormatted = "Wins"
+            if (msg[3] == "losses") type = "losses", typeFormatted = "Losses"
+            if (msg[3] == "wlr") type = "wlr", typeFormatted = "WLR"
+            if (msg[3] == "fkdr") type = "fkdr", typeFormatted = "FKDR"
+            if (["kills", "k"].includes(msg[3])) type = "kills", typeFormatted = "Kills"
+            if (["deaths", "d"].includes(msg[3])) type = "deaths", typeFormatted = "Deaths"
+            if (msg[3] == "kdr") type = "kdr", typeFormatted = "KDR"
 
             this.send(
-              `/gc [Guild] ${timeframe} ${mode} ${gamemodeFormatted} ${typeFormatted} » #1 ${await getUsername(data[gamemode][type][0].UUID)}: ${data[gamemode][type][0][type]} | #2 ${await getUsername(data[gamemode][type][1].UUID)}: ${data[gamemode][type][1][type]} | #3 ${await getUsername(data[gamemode][type][2].UUID)}: ${data[gamemode][type][2][type]} | #4 ${await getUsername(data[gamemode][type][3].UUID)}: ${data[gamemode][type][3][type]} | #5 ${await getUsername(data[gamemode][type][4].UUID)}: ${
-                data[gamemode][type][4][type]
+              `/gc [Guild] ${timeframe} ${mode} ${gamemodeFormatted} ${typeFormatted} » #1 ${await getUsername(data[gamemode][type][0].UUID)}: ${data[gamemode][type][0][type]} | #2 ${await getUsername(data[gamemode][type][1].UUID)}: ${data[gamemode][type][1][type]} | #3 ${await getUsername(data[gamemode][type][2].UUID)}: ${data[gamemode][type][2][type]} | #4 ${await getUsername(data[gamemode][type][3].UUID)}: ${data[gamemode][type][3][type]} | #5 ${await getUsername(data[gamemode][type][4].UUID)}: ${data[gamemode][type][4][type]
               } | #6 ${await getUsername(data[gamemode][type][5].UUID)}: ${data[gamemode][type][5][type]} | #7 ${await getUsername(data[gamemode][type][6].UUID)}: ${data[gamemode][type][6][type]} | #8 ${await getUsername(data[gamemode][type][7].UUID)}: ${data[gamemode][type][7][type]} | #9 ${await getUsername(data[gamemode][type][8].UUID)}: ${data[gamemode][type][8][type]} | #10 ${await getUsername(data[gamemode][type][9].UUID)}: ${data[gamemode][type][9][type]}`
             )
           } else if (mode.toLowerCase() == "general") {
             // Check Stat Type
-            if (msg[2] == "level") {
-              var type = "level"
-              var typeFormatted = "Level"
-            }
-            if (msg[2] == "karma") {
-              var type = "karma"
-              var typeFormatted = "Karma"
-            }
-            if (["quest", "quests"].includes(msg[2])) {
-              var type = "quests"
-              var typeFormatted = "Quests"
-            }
-            if (["points", "achievementpoints", "ap"].includes(msg[2])) {
-              var type = "achievementPoints"
-              var typeFormatted = "Achievement Points"
-            }
+            if (msg[2] == "level") type = "level", typeFormatted = "Level"
+            if (msg[2] == "karma") type = "karma", typeFormatted = "Karma"
+            if (["quest", "quests"].includes(msg[2])) type = "quests", typeFormatted = "Quests"
+            if (["points", "achievementpoints", "ap"].includes(msg[2])) type = "achievementPoints", typeFormatted = "Achievement Points"
 
             this.send(
               `/gc [Guild] ${timeframe} ${mode} ${typeFormatted} » #1 ${await getUsername(data[type][0].UUID)}: ${data[type][0][type]} | #2 ${await getUsername(data[type][1].UUID)}: ${data[type][1][type]} | #3 ${await getUsername(data[type][2].UUID)}: ${data[type][2][type]} | #4 ${await getUsername(data[type][3].UUID)}: ${data[type][3][type]} | #5 ${await getUsername(data[type][4].UUID)}: ${data[type][4][type]} | #6 ${await getUsername(data[type][5].UUID)}: ${data[type][5][type]} | #7 ${await getUsername(
@@ -296,4 +140,4 @@ class guildLeaderboardCommand extends minecraftCommand {
   }
 }
 
-module.exports = guildLeaderboardCommand
+module.exports = GuildLeaderboardCommand
