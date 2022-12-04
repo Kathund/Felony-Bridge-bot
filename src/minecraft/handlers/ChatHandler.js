233e652bd35fb8213881c97cae4e31394f6f6905
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-04 16:48:13 +0800

    Update ChatHandler.js

--- src/minecraft/handlers/ChatHandler.js
+++ src/minecraft/handlers/ChatHandler.js
@@ -156,6 +156,7 @@ class StateHandler extends eventHandler {
           `/oc [${player.rank}] ${player.nickname}: ${meetRequirements ? "has" : "hasnt"
           } the requirements to join ${config.minecraft.guild.name}!`
         );
+        console.log(playere.plusColor)
         const statsEmbed = new EmbedBuilder()
           .setColor(`${meetRequirements ? "0x1FFF4C" : "0xf92121"}`)
           .setTitle(
