fde85aaed29b0a21ab22d5684416e8d9f45a7a2a
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-05 10:04:26 +0800

    Added colors back to logger.js

--- src/Logger.js
+++ src/Logger.js
@@ -1,21 +1,23 @@
+var cli = require("cli-color");
+
 async function discordMessage(message) {
-  return console.log(`[${await getCurrentTime()}] Discord > ${message}`);
+  return console.log(cli.bgMagenta.black(`[${await getCurrentTime()}] Discord >`) + ' ' + cli.magenta(message));
 }
 
 async function minecraftMessage(message) {
-  return console.log(`[${await getCurrentTime()}] Minecraft > ${message}`);
+  return console.log(cli.bgGreenBright.black(`[${await getCurrentTime()}] Minecraft >`) + ' ' + cli.greenBright(message));
 }
 
 async function warnMessage(message) {
-  return console.log(`[${await getCurrentTime()}] Warning > ${message}`)
+  return console.log(cli.bgGreenBright.black(`[${await getCurrentTime()}] Warning >`) + ' ' + cli.yellow(message));
 }
 
 async function errorMessage(message) {
-  return console.log(`[${await getCurrentTime()}] Error > ${message}`)
+  return console.log(cli.bgRedBright.black(`[${await getCurrentTime()}] Error >`) + ' ' + cli.redBright(message));
 }
 
 async function broadcastMessage(message, location) {
-  return console.log(`[${await getCurrentTime()}] ${location} Broadcast > ${message}`);
+  return console.log(cli.inverse(`[${await getCurrentTime()}] ${location} Broadcast >`) + ' ' + (message));
 }
 
 async function getCurrentTime() {
