6fcf8f3635470454599b492e9c16ec362a9d8900
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-07 17:03:01 +0800

    Update apiCommand.js

--- src/minecraft/commands/apiCommand.js
+++ src/minecraft/commands/apiCommand.js
@@ -1,5 +1,6 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const { addNotation } = require("../../contracts/helperFunctions.js");
+const { getUsername } = require("../../contracts/API/MojangAPI.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
 
 class APICommand extends minecraftCommand {
@@ -14,9 +15,8 @@ class APICommand extends minecraftCommand {
 
   async onCommand(username, message) {
     try {
-      var key = hypixel.getKeyInfo()
-      this.send(`/gc a `)
-      console.log(key)
+      var key = await hypixel.getKeyInfo()
+      this.send(`/gc Owned by ${await getUsername(key.owner)} - ${addNotation('oneLetters', key.totalRequests)} requests`)
     } catch (error) {
       console.log(error);
       this.send("/gc Something went wrong..");
