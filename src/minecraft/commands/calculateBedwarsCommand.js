ecf6f8d92aefc4d7cbe016af7bedf0f732cc5abc
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-07 11:14:08 +0800

    Update calculateBedwarsCommand.js

--- src/minecraft/commands/calculateBedwarsCommand.js
+++ src/minecraft/commands/calculateBedwarsCommand.js
@@ -21,13 +21,12 @@ class CalculateBedwarsCommand extends minecraftCommand {
 
                 // Todo add support for someones username and a gamemode (solo, doubles, threes, fours, 4v4)
                 if (['fkdr', 'wlr', 'blr'].includes(msg[0])) type = msg[0];
-                console.log(type)
                 if (msg[1]) target = msg[1];
 
                 var player = await hypixel.getPlayer(username);
 
                 if (type == 'fkdr') {
-                    if (target > player.stats.bedwars.finalKDRatio) { this.send(`/gc You already have a higher fkdr than ${target}`) }
+                    if (target < player.stats.bedwars.finalKDRatio) { this.send(`/gc You already have a higher fkdr than ${target}`) }
                     else {
                         var finalKills = player.stats.bedwars.finalKills;
                         var finalDeaths = player.stats.bedwars.finalDeaths;
@@ -35,15 +34,21 @@ class CalculateBedwarsCommand extends minecraftCommand {
                         this.send(`/gc You need ${neededFinalKills} final kills with 0 final deaths to reach ${target} fkdr`);
                     }
                 } else if (type == 'wlr') {
-                    var wins = player.stats.bedwars.wins;
-                    var losses = player.stats.bedwars.losses;
-                    var neededWins = (target * losses) - wins;
-                    this.send(`/gc You need ${neededWins} wins with 0 losses to reach ${target} wlr`);
+                    if (target < player.stats.bedwars.WLRatio) { this.send(`/gc You already have a higher wlr than ${target}`) }
+                    else {
+                        var wins = player.stats.bedwars.wins;
+                        var losses = player.stats.bedwars.losses;
+                        var neededWins = (target * losses) - wins;
+                        this.send(`/gc You need ${neededWins} wins with 0 losses to reach ${target} wlr`);
+                    }
                 } else if (type == 'blr') {
-                    var brokenBeds = player.stats.bedwars.beds.broken;
-                    var lostBeds = player.stats.bedwars.beds.lost;
-                    var neededBrokenBeds = (target * lostBeds) - brokenBeds;
-                    this.send(`/gc You need ${neededBrokenBeds} broken beds with 0 lost beds to reach ${target} blr`);
+                    if (target < player.stats.bedwars.beds.BLRatio) { this.send(`/gc You already have a higher fkdr than ${target}`) }
+                    else {
+                        var brokenBeds = player.stats.bedwars.beds.broken;
+                        var lostBeds = player.stats.bedwars.beds.lost;
+                        var neededBrokenBeds = (target * lostBeds) - brokenBeds;
+                        this.send(`/gc You need ${neededBrokenBeds} broken beds with 0 lost beds to reach ${target} blr`);
+                    }
                 }
             }
 
