c5b3d20fecb8f5e1703bfa04b588a15ac41506b3
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-10 11:14:59 +0800

    Update dailyCommand.js

--- src/minecraft/commands/dailyCommand.js
+++ src/minecraft/commands/dailyCommand.js
@@ -1,5 +1,5 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const { getStats } = require("../../contracts/helperFunctions.js");
 const { getUUID } = require("../../contracts/API/MojangAPI.js");
 const config = require("../../../config.json");
