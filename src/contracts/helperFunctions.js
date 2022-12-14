5b244554b7e842041e79b6b2129f30e77834e248
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-14 14:41:52 +0800

    Update helperFunctions.js

--- src/contracts/helperFunctions.js
+++ src/contracts/helperFunctions.js
@@ -68,7 +68,7 @@ function numberWithCommas(x) {
 
 function generateID(length) {
   let result = "";
-  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-=0123456789!@#$%^&*()~",
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()~",
     charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
