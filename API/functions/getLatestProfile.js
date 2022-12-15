ae47827dadf04a3d7e2f49f8fe8c3f5eab850e1d
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-15 15:39:57 +0800

    Update getLatestProfile.js

--- API/functions/getLatestProfile.js
+++ API/functions/getLatestProfile.js
@@ -1,11 +1,8 @@
-const { getUUID } = require("../../src/contracts/API/MojangAPI.js");
-const { parseHypixel } = require("../utils/hypixel.js");
 const { isUuid } = require("../utils/uuid.js");
 const config = require("../../config.json");
-const fetch = (...args) =>
-  import("node-fetch")
-    .then(({ default: fetch }) => fetch(...args))
-    .catch((err) => console.log(err));
+const { parseHypixel } = require("../utils/hypixel.js");
+const axios = require("axios");
+const { getUUID } = require("../../src/contracts/API/MojangAPI.js");
 
 async function getLatestProfile(uuid) {
   try {
@@ -16,31 +13,35 @@ async function getLatestProfile(uuid) {
         return error.response.data;
       }
     }
-    
-    fetch(`https://api.hypixel.net/player?key=${config.api.hypixelAPIkey}&uuid=${uuid}`).then((res) => res.json()).then((playerRes) => {
-      fetch(`https://api.hypixel.net/skyblock/profiles?key=${config.api.hypixelAPIkey}&uuid=${uuid}`).then((res) => res.json()).then((profileRes) => {
 
-        const player = parseHypixel(playerRes, uuid);
+    const [playerRes, profileRes] = await Promise.all([
+      axios.get(
+        `https://api.hypixel.net/player?key=${config.api.hypixelAPIkey}&uuid=${uuid}`
+      ),
+      axios.get(
+        `https://api.hypixel.net/skyblock/profiles?key=${config.api.hypixelAPIkey}&uuid=${uuid}`
+      ),
+    ]);
 
-        if (!profileRes.profiles) {
-          return {
-            status: 404,
-            reason: `Found no SkyBlock profiles for a user with a UUID of '${uuid}'.`,
-          };
-        }
+    const player = parseHypixel(playerRes, uuid);
 
-        const profileData = profileRes.profiles.find((a) => a.selected);
-        const profile = profileData.members[uuid];
+    if (!profileRes.data.profiles) {
+      return {
+        status: 404,
+        reason: `Found no SkyBlock profiles for a user with a UUID of '${uuid}'.`,
+      };
+    }
 
-        return {
-          profile: profile,
-          profileData: profileData,
-          playerRes: playerRes,
-          player: player,
-          uuid: uuid,
-        };
-      });
-    });
+    const profileData = profileRes.data.profiles.find((a) => a.selected);
+    const profile = profileData.members[uuid];
+
+    return {
+      profile: profile,
+      profileData: profileData,
+      playerRes: playerRes.data,
+      player: player,
+      uuid: uuid,
+    };
   } catch (error) {
     console.log(error);
     return { status: 404, reason: error };
