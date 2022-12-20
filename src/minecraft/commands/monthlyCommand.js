2b0ff2770a7b528e44145b605d98bb6994ad330f
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-20 11:39:19 +0800

    Update monthlyCommand.js

--- src/minecraft/commands/monthlyCommand.js
+++ src/minecraft/commands/monthlyCommand.js
@@ -307,7 +307,7 @@ class MonthlyStatsCommand extends minecraftCommand {
           } being added to the database..`
         );
 
-        this.send(register(uuid,player))
+        this.send(`/gc ${register(uuid,player)}`)
       }
     }
   }
