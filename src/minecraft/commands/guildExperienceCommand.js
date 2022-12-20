4e3fc8717374d4f31a87aa429aa26d71df27f355
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-20 11:39:31 +0800

    Update guildExperienceCommand.js

--- src/minecraft/commands/guildExperienceCommand.js
+++ src/minecraft/commands/guildExperienceCommand.js
@@ -1,11 +1,7 @@
+const { addCommas, register } = require("../../contracts/helperFunctions.js");
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
-const { addCommas } = require("../../contracts/helperFunctions.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
-const config = require("../../../config.json");
-const fetch = (...args) =>
-  import("node-fetch")
-    .then(({ default: fetch }) => fetch(...args))
-    .catch((err) => console.log(err));
+const { getUUID } = require("../../contracts/API/MojangAPI.js");
 
 class GuildEXPCommand extends minecraftCommand {
   constructor(minecraft) {
@@ -21,31 +17,11 @@ class GuildEXPCommand extends minecraftCommand {
     try {
       const arg = this.getArgs(message);
       if (arg[0]) username = arg[0];
-      var player = await hypixel.getPlayer(username);
       var guild = await hypixel.getGuild("player", username);
       var rawGexp = guild.me.weeklyExperience;
       var gexp = addCommas(rawGexp);
       this.send(`/gc ${username}'s GEXP is ${gexp}`);
-      fetch(
-        `${config.api.pixelicAPI}/player/register?key=${config.api.pixelicKey}&uuid=${player.uuid}`,
-        {
-          method: "POST",
-        }
-      ).then((res) => {
-        if (res.status == 201) {
-          console.log(
-            `Successfully registered ${player.nickname} in the database!`
-          );
-        } else if (res.status == 400) {
-          console.log(
-            `${player.nickname} is already registered in the database!`
-          );
-        } else {
-          console.log(
-            `An error occured while registering ${player.nickanem} in the database! Please try again in few seconds.`
-          );
-        }
-      });
+      console.log(register(username, await getUUID(username)))
     } catch (error) {
       console.log(error);
       this.send("/gc Something went wrong..");
