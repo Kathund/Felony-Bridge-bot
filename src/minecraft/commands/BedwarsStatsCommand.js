3efee56710bbcf124618804c945f5be94f408363
Kath <55346310+Kathund@users.noreply.github.com>
2022-12-21 15:10:32 +0800

    Update BedwarsStatsCommand.js

--- src/minecraft/commands/BedwarsStatsCommand.js
+++ src/minecraft/commands/BedwarsStatsCommand.js
@@ -18,23 +18,33 @@ class BedwarsCommand extends minecraftCommand {
     try {
       const msg = this.getArgs(message);
       let mode = null;
+      let hidden = false;
 
-      if (["solo", "doubles", "threes", "fours", "4v4", "castle", "ultimate", "rush", "armed", "lucky", "voidless"].includes(msg[0])) {
+      if (["solo", "doubles", "threes", "fours", "4v4", "castle", "ultimate", "rush", "armed", "lucky", "voidless", "overall", "all"].includes(msg[0])) {
         mode = msg[0];
         if (msg[1] && !msg[1].includes("/")) {
           username = msg[1];
+          if (msg[2] == "hidden") hidden = true;
+        }
+        if (msg[1] == "hidden") {
+          hidden = true;
+          if (msg[2] && !msg[2].includes("/")) username = msg[2];
         }
       } else {
-        if (msg[0] && !msg[0].includes("/")) username = msg[0];
-        if (["solo", "doubles", "threes", "fours", "4v4", "castle", "ultimate", "rush", "armed", "lucky", "voidless"].includes(msg[1])) mode = msg[1];
+        if (msg[0] && !msg[0].includes("/")) {
+          username = msg[0];
+          if (["solo", "doubles", "threes", "fours", "4v4", "castle", "ultimate", "rush", "armed", "lucky", "voidless", "overall", "all"].includes(msg[1])) mode = msg[1];
+          if (["solo", "doubles", "threes", "fours", "4v4", "castle", "ultimate", "rush", "armed", "lucky", "voidless", "overall", "all"].includes(msg[2])) mode = msg[2];
+          if (msg[1] == "hidden") hidden = true;
+          if (msg[2] == "hidden") hidden = true;
+        }
       }
 
       const player = await hypixel.getPlayer(username);
       var star = getStar(player.stats.bedwars.level);
 
       if (!mode || ["overall", "all"].includes(mode)) {
-        this.send(
-          `/gc [${player.stats.bedwars.level}${star}] ${player.nickname
+        this.send(`${hidden ? "/oc" : "/gc"} [${player.stats.bedwars.level}${star}] ${player.nickname
           } | Coins: ${addNotation("oneLetters", player.stats.bedwars.coins)
           } | FK: ${addCommas(player.stats.bedwars.finalKills)} FKDR: ${player.stats.bedwars.finalKDRatio
           } | Wins: ${addNotation("oneLetters", player.stats.bedwars.wins)} WLR: ${player.stats.bedwars.WLRatio
@@ -43,7 +53,7 @@ class BedwarsCommand extends minecraftCommand {
           } | Items Collected: Iron: ${addNotation("oneLetters", player.stats.bedwars.collectedItemsTotal.iron)} Gold: ${addNotation("oneLetters", player.stats.bedwars.collectedItemsTotal.gold)} Diamonds: ${addNotation("oneLetters", player.stats.bedwars.collectedItemsTotal.diamond)} Emeralds: ${addNotation("oneLetters", player.stats.bedwars.collectedItemsTotal.emerald)}`
         );
       } else if (["solo", "doubles", "threes", "fours", "4v4", "castle"].includes(mode)) {
-        this.send(`/gc [${player.stats.bedwars.level}${star}] ${player.nickname
+        this.send(`${hidden ? "/oc" : "/gc"} [${player.stats.bedwars.level}${star}] ${player.nickname
           } | ${capitalize(mode)
           } | FK: ${addCommas(player.stats.bedwars[mode].finalKills)} FKDR: ${player.stats.bedwars[mode].finalKDRatio
           } | Wins: ${addCommas(player.stats.bedwars[mode].wins)} WLR: ${player.stats.bedwars[mode].WLRatio
@@ -58,7 +68,7 @@ class BedwarsCommand extends minecraftCommand {
         var losses = player.stats.bedwars.dream[mode].doubles.losses + player.stats.bedwars.dream[mode].fours.losses
         var bedsBroken = player.stats.bedwars.dream[mode].doubles.beds.broken + player.stats.bedwars.dream[mode].fours.beds.broken
         var bedsLost = player.stats.bedwars.dream[mode].doubles.beds.lost + player.stats.bedwars.dream[mode].fours.beds.lost
-        this.send(`/gc [${player.stats.bedwars.level}${star}] ${player.nickname
+        this.send(`${hidden ? "/oc" : "/gc"} [${player.stats.bedwars.level}${star}] ${player.nickname
           } | ${capitalize(mode)
           } | FK: ${addCommas(finalKills)} FKDR: ${(finalKills / finalDeaths).toFixed(2)
           } | Wins: ${addCommas(wins)} WLR: ${(wins / losses).toFixed(2)
