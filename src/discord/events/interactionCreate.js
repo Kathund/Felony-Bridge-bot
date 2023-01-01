3f665d33c87d7ca0337d913f204fdaf3c4c32070
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-01 21:10:16 +0800

    Update interactionCreate.js

--- src/discord/events/interactionCreate.js
+++ src/discord/events/interactionCreate.js
@@ -1,19 +1,18 @@
-// eslint-disable-next-line
-const { logError} = require("../../contracts/helperFunctions.js")
+const { logError } = require("../../contracts/helperFunctions.js")
 const config = require("../../../config.json")
-const Logger = require("../.././Logger");
+const logger = require("../.././Logger.js");
 
 module.exports = {
   name: "interactionCreate",
   async execute(interaction) {
     if (interaction.isChatInputCommand()) {
-      await interaction.deferReply({ ephemeral: false }).catch(() => {});
+      await interaction.deferReply({ ephemeral: false }).catch(() => { });
 
       const command = interaction.client.commands.get(interaction.commandName);
       if (!command) return;
 
       try {
-        Logger.discordMessage(`${interaction.user.username} - [${interaction.commandName}]`);
+        logger.discordMessage(`${interaction.user.username} - [${interaction.commandName}]`);
 
         bridgeChat = interaction.channelId;
 
@@ -21,7 +20,7 @@ module.exports = {
       } catch (error) {
         await logError(config.minecraft.bot.name, error)
         console.log(error);
-        
+
         await interaction.reply({
           content: "There was an error while executing this command!",
           ephemeral: true,
