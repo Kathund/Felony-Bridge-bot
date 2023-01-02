d83cd7abbe6f15338ccc89f4ec3badef7bc788a9
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-02 13:59:05 +0800

    Update DenickerCommand.js

--- src/minecraft/commands/DenickerCommand.js
+++ src/minecraft/commands/DenickerCommand.js
@@ -1,6 +1,6 @@
+const { register, logError } = require("../../contracts/helperFunctions.js");
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
-const { register, logError } = require("../../contracts/helperFunctions.js");
 const { getUUID } = require("../../contracts/API/MojangAPI.js");
 const config = require("../../../config.json");
 const axios = require("axios");
@@ -41,7 +41,8 @@ class DenickerCommand extends minecraftCommand {
       );
       await register(await getUUID(username), username)
     } catch (error) {
-      await logError(error, username)
+      await logError(username, error);
+      console.log(error)
       this.send("/gc Sorry, I wasn't able to denick this person.");
     }
   }
