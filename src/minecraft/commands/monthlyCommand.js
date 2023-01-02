1a8d3f3be88fb6371266e608cb899c09c88d524c
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-02 14:03:01 +0800

    Update monthlyCommand.js

--- src/minecraft/commands/monthlyCommand.js
+++ src/minecraft/commands/monthlyCommand.js
@@ -1,5 +1,5 @@
-const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const { addCommas, register, logError } = require("../../contracts/helperFunctions.js");
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
 const { getUUID } = require("../../contracts/API/MojangAPI.js");
 const config = require("../../../config.json");
@@ -82,7 +82,7 @@ class MonthlyStatsCommand extends minecraftCommand {
     super(minecraft);
 
     this.name = "monthly";
-    this.aliases = [""];
+    this.aliases = [];
     this.description = "Get your monthly stats";
     this.options = ["name", "gamemode"];
     this.optionsDescription = ["Minecraft Username", "Hypixel Gamemode"];
@@ -300,7 +300,7 @@ class MonthlyStatsCommand extends minecraftCommand {
         })
       })
     } catch (error) {
-      await logError(error, username);
+      await logError(username, error);
       console.log(error)
       if (error.response?.data?.error == "Player not in database") {
         this.send(
