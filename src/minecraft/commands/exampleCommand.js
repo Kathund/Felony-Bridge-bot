80632084d87cdffab880e6f839f94dfca5e43adc
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 12:30:43 +0800

    Update exampleCommand.js

--- src/minecraft/commands/exampleCommand.js
+++ src/minecraft/commands/exampleCommand.js
@@ -12,13 +12,14 @@ class ExampleCommand extends minecraftCommand {
   }
 
   async onCommand(username, message) {
+    let playerIGN = username
     try {
       console.log(`the username var represents the username of the person running the command - ${username}`)
       console.log(`the message var represents the full message from the user - ${message}`)
       console.log(`use this.send to send a message as the bot`)
       this.send(`/gc Check the console`)
     } catch (error) {
-      await logError(username, error);
+      await logError(playerIGN, error);
       console.log(error);
       this.send("/gc Something went wrong..");
     }
