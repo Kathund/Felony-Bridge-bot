370afc9971c25ec38e17efe3b074e447492c7d0e
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 17:47:40 +0800

    Update guildCommand.js

--- src/minecraft/commands/guildCommand.js
+++ src/minecraft/commands/guildCommand.js
@@ -13,7 +13,7 @@ class GuildInformationCommand extends minecraftCommand {
   }
 
   async onCommand(username, message) {
-    let playerIGN = username
+    var playerIGN = username
     try {
       const args = this.getArgs(message);
       const guildName = args.map((arg) => capitalize(arg)).join(" ");
