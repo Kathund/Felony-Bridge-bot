c35f35e5ffe841cf97d8c9161976622548ffe67a
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 12:30:19 +0800

    Update dailyCommand.js

--- src/minecraft/commands/dailyCommand.js
+++ src/minecraft/commands/dailyCommand.js
@@ -82,7 +82,7 @@ class DailyStatsCommand extends minecraftCommand {
     super(minecraft);
 
     this.name = "daily";
-    this.aliases = [""];
+    this.aliases = [];
     this.description = "Get your daily stats";
     this.options = ["name", "gamemode"];
     this.optionsDescription = ["Minecraft Username", "Hypixel Gamemode"];
@@ -90,6 +90,7 @@ class DailyStatsCommand extends minecraftCommand {
 
 
   async onCommand(username, message) {
+    let playerIGN = username
     const args = this.getArgs(message);
     let mode,
       player = username;
@@ -301,7 +302,7 @@ class DailyStatsCommand extends minecraftCommand {
         })
       })
     } catch (error) {
-      await logError(username, error);
+      await logError(playerIGN, error);
       console.log(error)
       if (error.response?.data?.error == "Player not in database") {
         this.send(
@@ -310,7 +311,7 @@ class DailyStatsCommand extends minecraftCommand {
           } being added to the database..`
         );
 
-        this.send(`/gc ${register(uuid,player)}`)
+        this.send(`/gc ${register(uuid, player)}`)
       }
     }
   }
