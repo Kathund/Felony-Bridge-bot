d35076bd9b81247c509590500e164fff5cf13ebc
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 17:49:15 +0800

    Update woolwarsCommand.js

--- src/minecraft/commands/woolwarsCommand.js
+++ src/minecraft/commands/woolwarsCommand.js
@@ -25,7 +25,7 @@ class WoolwarsCommand extends minecraftCommand {
   }
 
   async onCommand(username, message) {
-    let playerIGN = username
+    var playerIGN = username
     try {
       const msg = this.getArgs(message);
       if (msg[0]) username = msg[0];
