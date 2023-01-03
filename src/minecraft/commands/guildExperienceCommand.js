3ba099aa2cc6b8c6f39d3947262b35b3c18d52af
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 17:47:52 +0800

    Update guildExperienceCommand.js

--- src/minecraft/commands/guildExperienceCommand.js
+++ src/minecraft/commands/guildExperienceCommand.js
@@ -14,7 +14,7 @@ class GuildEXPCommand extends minecraftCommand {
   }
 
   async onCommand(username, message) {
-    let playerIGN = username
+    var playerIGN = username
     try {
       const arg = this.getArgs(message);
       if (arg[0]) username = arg[0];
