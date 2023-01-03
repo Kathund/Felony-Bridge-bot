6260cd0b76374371bfd25b5d61aa154b45110b70
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 17:48:34 +0800

    Update murderMystoryCommand.js

--- src/minecraft/commands/murderMystoryCommand.js
+++ src/minecraft/commands/murderMystoryCommand.js
@@ -15,7 +15,7 @@ class MurderMysteryCommand extends minecraftCommand {
   }
 
   async onCommand(username, message) {
-    let playerIGN = username
+    var playerIGN = username
     try {
       const args = this.getArgs(message);
       if (args[0]) username = args[0];
