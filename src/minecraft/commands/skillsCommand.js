6f9ede663bd8247291820a8f174d855be0c974cb
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-03 14:02:58 +0800

    Update skillsCommand.js

--- src/minecraft/commands/skillsCommand.js
+++ src/minecraft/commands/skillsCommand.js
@@ -15,6 +15,7 @@ class SkillsCommand extends minecraftCommand {
   }
 
   async onCommand(username, message) {
+    let playerIGN = username
     try {
       const arg = this.getArgs(message);
       if (arg[0]) username = arg[0];
@@ -50,7 +51,7 @@ class SkillsCommand extends minecraftCommand {
         || 0}`
       );
     } catch (error) {
-      await logError(username, error);
+      await logError(playerIGN, error);
       console.log(error);
       this.send(
         "/gc There is no player with the given UUID or name or the player has no Skyblock profiles"
