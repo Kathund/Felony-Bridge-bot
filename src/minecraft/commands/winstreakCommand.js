1c8df48eff8a87e286e345f462560d7327304d41
Kath <55346310+Kathund@users.noreply.github.com>
2023-01-06 15:04:34 +0800

    Update winstreakCommand.js

--- src/minecraft/commands/winstreakCommand.js
+++ src/minecraft/commands/winstreakCommand.js
@@ -37,7 +37,7 @@ class DenickerCommand extends minecraftCommand {
         await register(await getUUID(username), username)
       });
     } catch (error) {
-      await logError(playerIGN, error);
+      await logError(playerIGN, error, this.name, message);
       console.log(error);
       this.send("/gc Something went wrong..");
     }
